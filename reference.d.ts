c805dae4905d95b4f4ab8dc7f31815fddb758d79
Alberto Gonzalez <algonzal@uji.es>
2020-01-24 11:43:28 +0100

    Add google play services location dependency

--- reference.d.ts
+++ reference.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="node_modules/tns-platform-declarations/android-29.d.ts" />
+/// <reference path="native_declarations/android-play-services-location.d.ts" />
+
 /// <reference path="node_modules/tns-platform-declarations/ios.d.ts" />
