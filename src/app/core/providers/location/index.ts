a8de37804af4daf29e690c482e720db92d290d1b
Miguel <al341802@uji.es>
2020-01-24 11:09:54 +0100

    Nativescript permissions plugin added

--- src/app/core/providers/location/index.ts
+++ src/app/core/providers/location/index.ts
@@ -3,6 +3,8 @@ import { Location } from './location';
 export interface NativeLocationProvider {
     isEnabled(): Promise<boolean>;
     enable(): void;
+    hasPermission(): boolean;
+    requestPermission(): Promise<void>;
     next(quantity: number): [Promise<Array<Location>>, ProviderInterruption];
 }
 
--- src/app/core/providers/location/location-provider.android.ts
+++ src/app/core/providers/location/location-provider.android.ts
@@ -8,6 +8,12 @@ export class AndroidLocationProvider implements NativeLocationProvider {
     enable(): void {
         throw new Error('Method not implemented.');
     }
+    hasPermission(): boolean {
+        throw new Error('Method not implemented.');
+    }
+    requestPermission(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
     next(quantity: number): [Promise<Array<Location>>, ProviderInterruption] {
         throw new Error('Method not implemented.');
     }
