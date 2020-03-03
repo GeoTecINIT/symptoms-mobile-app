32f34d4c984caac160b4d5130fdb6419e82ec908
Alberto Gonzalez <algonzal@uji.es>
2020-03-03 16:49:11 +0100

    Update production logger to use crashlytics

--- src/app/core/utils/logger/prod.ts
+++ src/app/core/utils/logger/prod.ts
@@ -1,19 +1,62 @@
 import { Logger } from './common';
+import { FirebaseManager, firebaseInstance } from '../firebase';
+import { DevLogger } from './dev';
+import { isAndroid } from 'tns-core-modules/platform';
 
 export class ProdLogger extends Logger {
+    constructor(
+        tag: string,
+        private firebase: FirebaseManager = firebaseInstance,
+        private auxLogger: Logger = new DevLogger(tag)
+    ) {
+        super(tag);
+    }
+
     protected logDebug(message: string): void {
-        throw new Error('Method not implemented.');
+        return; // Do not print or send debug messages in production
     }
 
-    protected logInfo(message: string): void {
-        throw new Error('Method not implemented.');
+    protected async logInfo(message: string): Promise<void> {
+        const crashlytics = await this.firebase.crashlytics();
+        if (crashlytics) {
+            crashlytics.log(message);
+        } else {
+            this.auxLogger.info(message);
+        }
     }
 
-    protected logWarning(message: string): void {
-        throw new Error('Method not implemented.');
+    protected async logWarning(message: string): Promise<void> {
+        const crashlytics = await this.firebase.crashlytics();
+        if (crashlytics) {
+            crashlytics.log(message);
+        } else {
+            this.auxLogger.warn(message);
+        }
     }
 
-    protected logError(message: string): void {
-        throw new Error('Method not implemented.');
+    protected async logError(message: string): Promise<void> {
+        const crashlytics = await this.firebase.crashlytics();
+        if (!crashlytics) {
+            this.auxLogger.error(message);
+
+            return;
+        }
+
+        if (isAndroid) {
+            crashlytics.sendCrashLog(new java.lang.Exception(message));
+        } else {
+            crashlytics.sendCrashLog(
+                new NSError({
+                    domain: 'es.uji.geotec.symptomsapp',
+                    code: 42,
+                    userInfo: NSDictionary.dictionaryWithObjectsAndKeys<
+                        string,
+                        string
+                    >({
+                        NSLocalizedUserMessage: message
+                    })
+                })
+            );
+        }
     }
 }
