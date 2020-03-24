b5cf8c0a01d75bf6cb4e7b2c7a9516c5aecd33ec
Alberto González <alberto.gonzalez@uji.es>
2020-03-24 13:27:25 +0100

    Add experiment management user interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
-import { DataService, DataItem } from '../shared/data.service';
-import { taskGraph } from '../core/tasks/graph/loader';
-import { emit, createEvent } from '../core/events';
 import { firebaseManager } from '../core/utils/firebase';
+
+import { Experiment, currentExperiment } from '../experiment';
 
 @Component({
     selector: 'Home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    items: Array<DataItem>;
-
-    constructor(private _itemService: DataService) {}
+    experiment: Experiment;
 
     ngOnInit(): void {
-        this.items = this._itemService.getItems();
-
+        this.experiment = currentExperiment.get();
         // FIXME: Ask user consent before doing this
         if (!firebaseManager.dataCollectionEnabled) {
             firebaseManager.enableUsageDataCollection();
@@ -24,20 +21,15 @@ export class HomeComponent implements OnInit {
     }
 
     onTapStart() {
-        this.emitStartEvent();
+        if (!this.experiment.name) {
+            alert('Please, introduce a name for the experiment');
+
+            return;
+        }
+        this.experiment = currentExperiment.start(this.experiment.name);
     }
 
     onTapStop() {
-        emit(createEvent('stopEvent'));
-    }
-
-    // FIXME: This has to be better handled with a view informing the user
-    // about the permissions to be asked
-    private async emitStartEvent() {
-        const isReady = await taskGraph.isReady();
-        if (!isReady) {
-            await taskGraph.prepare();
-        }
-        emit(createEvent('startEvent'));
+        this.experiment = currentExperiment.finish();
     }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,21 +1,18 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
-import { HomeRoutingModule } from "./home-routing.module";
-import { HomeComponent } from "./home.component";
-import { ItemDetailComponent } from "./item-detail/item-detail.component";
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { NativeScriptFormsModule } from 'nativescript-angular';
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
+        NativeScriptFormsModule,
         HomeRoutingModule
     ],
-    declarations: [
-        HomeComponent,
-        ItemDetailComponent
-    ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+    declarations: [HomeComponent, ItemDetailComponent],
+    schemas: [NO_ERRORS_SCHEMA]
 })
-export class HomeModule { }
+export class HomeModule {}
