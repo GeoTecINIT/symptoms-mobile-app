499eb5dcc9ae49d8fc50de18fc91b7dd71a397a0
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-03-30 18:37:34 +0200

    Migrate app component to new nested views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,32 +6,28 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 const routes: Routes = [
     {
         path: "",
-        redirectTo: "/(homeTab:home/default//browseTab:browse/default//searchTab:search/default)",
-        pathMatch: "full"
+        redirectTo: "welcome",
+        pathMatch: "full",
     },
 
     {
-        path: "home",
+        path: "welcome",
         component: NSEmptyOutletComponent,
-        loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule),
-        outlet: "homeTab"
+        loadChildren: () =>
+            import("./views/welcome/welcome.module").then(
+                (m) => m.WelcomeModule
+            ),
     },
     {
-        path: "browse",
+        path: "main",
         component: NSEmptyOutletComponent,
-        loadChildren: () => import("~/app/browse/browse.module").then((m) => m.BrowseModule),
-        outlet: "browseTab"
+        loadChildren: () =>
+            import("./views/main/main.module").then((m) => m.MainModule),
     },
-    {
-        path: "search",
-        component: NSEmptyOutletComponent,
-        loadChildren: () => import("~/app/search/search.module").then((m) => m.SearchModule),
-        outlet: "searchTab"
-    }
 ];
 
 @NgModule({
     imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
+    exports: [NativeScriptRouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
