0ab4df0e3de94d71294def0e15cdbf6d1bb6d5c7
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-03-30 18:27:15 +0200

    Add main view content list and module

---
+++ src/app/views/main/content/content-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "SymContentList",
+    templateUrl: "./content-list.component.html",
+    styleUrls: ["./content-list.component.scss"],
+})
+export class ContentListComponent implements OnInit {
+    constructor() {
+        // Initialize dependencies here
+    }
+
+    ngOnInit() {
+        // Use initialized dependencies
+    }
+}
