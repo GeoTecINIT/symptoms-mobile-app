cbf08de5ae277a8ab734bcd9d84f8c787a8bb7d1
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-03-30 18:28:48 +0200

    Add main view simulation actions list and module

---
+++ src/app/views/main/simulation/simulation-actions.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "SymSimulationActions",
+    templateUrl: "./simulation-actions.component.html",
+    styleUrls: ["./simulation-actions.component.scss"],
+})
+export class SimulationActionsComponent implements OnInit {
+    constructor() {
+        // Initialize dependencies here
+    }
+
+    ngOnInit() {
+        // Use initialized dependencies
+    }
+}
