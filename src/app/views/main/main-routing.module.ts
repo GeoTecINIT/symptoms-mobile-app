8cdfb5f51c6e67689696f2bb7b65b5557ac6d239
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-03-30 18:34:54 +0200

    Add main view component with bottom navigation

--- src/app/views/main/main-routing.module.ts
+++ src/app/views/main/main-routing.module.ts
@@ -1,8 +1,44 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
+import { NSEmptyOutletComponent } from "nativescript-angular";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { MainComponent } from "~/app/views/main/main.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "default",
+        component: MainComponent,
+        children: [
+            {
+                path: "progress",
+                component: NSEmptyOutletComponent,
+                loadChildren: () =>
+                    import("./progress/progress.module").then(
+                        (m) => m.ProgressModule
+                    ),
+                outlet: "progressTab",
+            },
+            {
+                path: "content",
+                component: NSEmptyOutletComponent,
+                loadChildren: () =>
+                    import("./content/content.module").then(
+                        (m) => m.ContentModule
+                    ),
+                outlet: "contentTab",
+            },
+            {
+                path: "simulation",
+                component: NSEmptyOutletComponent,
+                loadChildren: () =>
+                    import("./simulation/simulation.module").then(
+                        (m) => m.SimulationModule
+                    ),
+                outlet: "simulationTab",
+            },
+        ],
+    },
+];
 
 @NgModule({
     imports: [NativeScriptRouterModule.forChild(routes)],
