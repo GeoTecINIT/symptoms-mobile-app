778594ca9c761e1d0b64759f1c14855dc3fd1baf
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 18:18:30 +0200

    Create record list component

---
+++ src/app/views/main/pages/progress/pages/records-list/records-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "SymRecordsList",
+    templateUrl: "./records-list.component.html",
+    styleUrls: ["./records-list.component.scss"],
+})
+export class RecordsListComponent implements OnInit {
+    constructor() {
+        // Initialize dependencies here
+    }
+
+    ngOnInit() {
+        // Use initialized dependencies
+    }
+}
