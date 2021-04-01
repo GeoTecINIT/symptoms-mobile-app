f1ad13c6acc2dc0b9f23af15ac6d0efacae5cfe6
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 18:48:55 +0200

    Update welcome section routing

--- src/app/views/welcome/welcome-routing.module.ts
+++ src/app/views/welcome/welcome-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { LoginComponent } from "./login/login.component";
+import { TutorialComponent } from "./tutorial/tutorial.component";
+import { SetupConfirmationComponent } from "./setup-confirmation/setup-confirmation.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
     { path: "login", component: LoginComponent },
+    { path: "tutorial", component: TutorialComponent },
+    { path: "setup-confirmation", component: SetupConfirmationComponent },
 ];
 
 @NgModule({
