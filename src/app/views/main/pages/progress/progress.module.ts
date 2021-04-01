94217f88fb0b74838f548d9ee4e328525e24c605
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 18:19:01 +0200

    Create aggregate list component

--- src/app/views/main/pages/progress/progress.module.ts
+++ src/app/views/main/pages/progress/progress.module.ts
@@ -4,21 +4,27 @@ import { ProgressRoutingModule } from "./progress-routing.module";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { CommonMainModule } from "../../common/common-main.module";
+import { CommonComponentsModule } from "~/app/views/common/common-components.module";
 
 import { ProgressContainerComponent } from "./progress-container.component";
 import { IdleProgressComponent } from "./idle-progress/idle-progress.component";
 import { UnderExposureComponent } from "./under-exposure/under-exposure.component";
+import { RecordsListComponent } from "./pages/records-list/records-list.component";
+import { AggregateListComponent } from "./pages/aggregate-list/aggregate-list.component";
 
 @NgModule({
     imports: [
         ProgressRoutingModule,
         NativeScriptCommonModule,
         CommonMainModule,
+        CommonComponentsModule,
     ],
     declarations: [
         ProgressContainerComponent,
         IdleProgressComponent,
         UnderExposureComponent,
+        RecordsListComponent,
+        AggregateListComponent,
     ],
     schemas: [NO_ERRORS_SCHEMA],
 })
