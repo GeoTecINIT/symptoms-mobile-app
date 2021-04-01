6a2d490df0e18bdb7699e7a98c247c12228bc01d
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 19:43:15 +0200

    Create content view modal

---
+++ src/app/views/main/modals/content-view/content-view-modal.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+    selector: "SymContentViewModal",
+    templateUrl: "./content-view-modal.component.html",
+    styleUrls: ["./content-view-modal.component.scss"],
+})
+export class ContentViewModalComponent implements OnInit {
+    constructor(
+        private routerExtension: RouterExtensions,
+        private activeRoute: ActivatedRoute
+    ) {}
+
+    ngOnInit() {
+        this.routerExtension
+            .navigate([{ outlets: { contentViewModal: ["content-view"] } }], {
+                relativeTo: this.activeRoute,
+            })
+            .catch((e) =>
+                console.error("Could not navigate to content view:", e)
+            );
+    }
+}
---
+++ src/app/views/main/modals/content-view/content-view.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { CommonComponentsModule } from "~/app/views/common/common-components.module";
+
+import { ContentViewRoutingModule } from "./content-view-routing.module";
+
+import { ContentViewModalComponent } from "./content-view-modal.component";
+import { ContentViewContainerComponent } from "./content-view-container/content-view-container.component";
+
+@NgModule({
+    imports: [
+        NativeScriptCommonModule,
+        CommonComponentsModule,
+        ContentViewRoutingModule,
+    ],
+    declarations: [ContentViewModalComponent, ContentViewContainerComponent],
+    entryComponents: [ContentViewModalComponent],
+    schemas: [NO_ERRORS_SCHEMA],
+})
+export class ContentViewModule {}
