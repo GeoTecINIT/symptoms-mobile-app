b3f07627a4bda5ee5adcd045d3a32ce9a4e8fdbe
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 14:26:05 +0200

    Include main action bar in simulation page

--- src/app/views/main/simulation/simulation.module.ts
+++ src/app/views/main/simulation/simulation.module.ts
@@ -4,10 +4,15 @@ import { SimulationRoutingModule } from "./simulation-routing.module";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { SimulationActionsComponent } from "./simulation-actions.component";
+import { CommonMainModule } from "~/app/views/main/common/common-main.module";
 
 @NgModule({
     declarations: [SimulationActionsComponent],
-    imports: [SimulationRoutingModule, NativeScriptCommonModule],
+    imports: [
+        SimulationRoutingModule,
+        NativeScriptCommonModule,
+        CommonMainModule,
+    ],
     schemas: [NO_ERRORS_SCHEMA],
 })
 export class SimulationModule {}
