f8cadaaa7252bd50fd175a9bfb85cfe71403971c
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 18:17:36 +0200

    Create common slide navigation button

--- src/app/views/common/common-components.module.ts
+++ src/app/views/common/common-components.module.ts
@@ -1,13 +1,23 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { CloseActionBarComponent } from "./close-action-bar/close-action-bar.component";
 import { BackActionBarComponent } from "./back-action-bar/back-action-bar.component";
+import { SlideNavigationButtonComponent } from "./slide-navigation-button/slide-navigation-button.component";
 
 @NgModule({
-    imports: [NativeScriptCommonModule],
-    declarations: [CloseActionBarComponent, BackActionBarComponent],
+    imports: [NativeScriptCommonModule, NativeScriptRouterModule],
+    declarations: [
+        CloseActionBarComponent,
+        BackActionBarComponent,
+        SlideNavigationButtonComponent,
+    ],
     schemas: [NO_ERRORS_SCHEMA],
-    exports: [CloseActionBarComponent, BackActionBarComponent],
+    exports: [
+        CloseActionBarComponent,
+        BackActionBarComponent,
+        SlideNavigationButtonComponent,
+    ],
 })
 export class CommonComponentsModule {}
