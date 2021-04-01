95f58acdd5576da33b8f79dc43a159c1b1dc79b2
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 18:19:01 +0200

    Create aggregate list component

---
+++ src/app/views/main/pages/progress/pages/aggregate-list/aggregate-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "SymAggregateList",
+    templateUrl: "./aggregate-list.component.html",
+    styleUrls: ["./aggregate-list.component.scss"],
+})
+export class AggregateListComponent implements OnInit {
+    constructor() {
+        // Initialize dependencies here
+    }
+
+    ngOnInit() {
+        // Use initialized dependencies
+    }
+}
