a0f60a55cb8b10cf82ff2d4b0997e8f7d99661bf
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 14:20:34 +0200

    Create settings modal service for modal actions

---
+++ src/app/views/main/settings/settings-modal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { SettingsModule } from "./settings.module";
+import { MainViewService } from "../main-view.service";
+import { SettingsModalComponent } from "./settings-modal.component";
+
+@Injectable({
+    providedIn: SettingsModule,
+})
+export class SettingsModalService {
+    constructor(private mainViewService: MainViewService) {}
+
+    show() {
+        this.mainViewService
+            .showFullScreenAnimatedModal(SettingsModalComponent)
+            .catch((e) =>
+                console.error(`Error while opening settings modal:`, e)
+            );
+    }
+}
