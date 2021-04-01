acd52887922edb89ed46a001e072238b4c1406fc
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 19:44:30 +0200

    Update common main module with content view module import

--- src/app/views/main/common/common-main.module.ts
+++ src/app/views/main/common/common-main.module.ts
@@ -1,13 +1,19 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
-import { MainActionBarComponent } from "./main-action-bar/main-action-bar.component";
-
 import { SettingsModule } from "../modals/settings/settings.module";
 import { NotificationsModule } from "../modals/notifications/notifications.module";
+import { ContentViewModule } from "../modals/content-view/content-view.module";
+
+import { MainActionBarComponent } from "./main-action-bar/main-action-bar.component";
 
 @NgModule({
-    imports: [NativeScriptCommonModule, SettingsModule, NotificationsModule],
+    imports: [
+        NativeScriptCommonModule,
+        SettingsModule,
+        NotificationsModule,
+        ContentViewModule,
+    ],
     declarations: [MainActionBarComponent],
     exports: [MainActionBarComponent],
     schemas: [NO_ERRORS_SCHEMA],
