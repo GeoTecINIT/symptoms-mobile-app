b18ce037753df134527d64832faafb99a890b178
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 14:25:51 +0200

    Include main action bar in progress page

--- src/app/views/main/progress/progress.module.ts
+++ src/app/views/main/progress/progress.module.ts
@@ -4,10 +4,15 @@ import { ProgressRoutingModule } from "./progress-routing.module";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { ProgressContainerComponent } from "./progress-container.component";
+import { CommonMainModule } from "~/app/views/main/common/common-main.module";
 
 @NgModule({
     declarations: [ProgressContainerComponent],
-    imports: [ProgressRoutingModule, NativeScriptCommonModule],
+    imports: [
+        ProgressRoutingModule,
+        NativeScriptCommonModule,
+        CommonMainModule,
+    ],
     schemas: [NO_ERRORS_SCHEMA],
 })
 export class ProgressModule {}
