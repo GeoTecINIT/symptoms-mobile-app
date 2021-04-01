41d4bc04321d6af6ac0c73249cb250710846d6ed
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 14:25:58 +0200

    Include main action bar in content page

--- src/app/views/main/content/content.module.ts
+++ src/app/views/main/content/content.module.ts
@@ -4,10 +4,11 @@ import { ContentRoutingModule } from "./content-routing.module";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { ContentListComponent } from "./content-list.component";
+import { CommonMainModule } from "~/app/views/main/common/common-main.module";
 
 @NgModule({
     declarations: [ContentListComponent],
-    imports: [ContentRoutingModule, NativeScriptCommonModule],
+    imports: [ContentRoutingModule, NativeScriptCommonModule, CommonMainModule],
     schemas: [NO_ERRORS_SCHEMA],
 })
 export class ContentModule {}
