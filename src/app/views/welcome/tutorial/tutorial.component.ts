f6e78526c8ffdc8b064152389a03d64df8ff9f18
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 18:48:44 +0200

    Create tutorial component

---
+++ src/app/views/welcome/tutorial/tutorial.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "SymTutorial",
+    templateUrl: "./tutorial.component.html",
+    styleUrls: ["./tutorial.component.scss"],
+})
+export class TutorialComponent implements OnInit {
+    constructor() {
+        // Initialize dependencies here
+    }
+
+    ngOnInit() {
+        // Use initialized dependencies
+    }
+}
--- src/app/views/welcome/welcome.module.ts
+++ src/app/views/welcome/welcome.module.ts
@@ -3,11 +3,23 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { WelcomeRoutingModule } from "./welcome-routing.module";
 
+import { CommonComponentsModule } from "~/app/views/common/common-components.module";
+
 import { LoginComponent } from "./login/login.component";
+import { TutorialComponent } from "./tutorial/tutorial.component";
+import { SetupConfirmationComponent } from "./setup-confirmation/setup-confirmation.component";
 
 @NgModule({
-    declarations: [LoginComponent],
-    imports: [NativeScriptCommonModule, WelcomeRoutingModule],
+    imports: [
+        NativeScriptCommonModule,
+        WelcomeRoutingModule,
+        CommonComponentsModule,
+    ],
+    declarations: [
+        LoginComponent,
+        TutorialComponent,
+        SetupConfirmationComponent,
+    ],
     schemas: [NO_ERRORS_SCHEMA],
 })
 export class WelcomeModule {}
