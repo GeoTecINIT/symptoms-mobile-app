b99440799cf3ad744b1c11d7d75d0c2c9971e68c
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 16:52:37 +0200

    Create notifications modal component

---
+++ src/app/views/main/modals/notifications/notifications-modal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+    selector: "Sym-notifications-modal",
+    templateUrl: "./notifications-modal.component.html",
+    styleUrls: ["./notifications-modal.component.scss"],
+})
+export class NotificationsModalComponent implements OnInit {
+    constructor(
+        private routerExtension: RouterExtensions,
+        private activeRoute: ActivatedRoute
+    ) {}
+
+    ngOnInit() {
+        this.routerExtension
+            .navigate(
+                [{ outlets: { notificationsModal: ["notifications"] } }],
+                { relativeTo: this.activeRoute }
+            )
+            .catch((e) =>
+                console.error("Could not navigate to notifications list:", e)
+            );
+    }
+}
