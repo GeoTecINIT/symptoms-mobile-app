c20bae6a7ed0812ebf1028c281c183a970acd15a
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 18:47:44 +0200

    Update slide navigation button to allow clearing history

--- src/app/views/common/slide-navigation-button/slide-navigation-button.component.ts
+++ src/app/views/common/slide-navigation-button/slide-navigation-button.component.ts
@@ -1,18 +1,13 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "SymSlideNavigationButton",
     templateUrl: "./slide-navigation-button.component.html",
     styleUrls: ["./slide-navigation-button.component.scss"],
 })
-export class SlideNavigationButtonComponent implements OnInit {
+export class SlideNavigationButtonComponent {
     @Input() text: string;
     @Input() route: Array<any>;
-    @Input() class: string;
-
-    ngOnInit() {
-        if (!this.class) {
-            this.class = "btn btn-primary";
-        }
-    }
+    @Input() class = "btn btn-primary";
+    @Input() clearHistory = false;
 }
