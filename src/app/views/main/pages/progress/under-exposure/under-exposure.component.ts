3835fbd4db47f8b0cc9b8a3c6a4a8e6d6bd5816d
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-01 17:45:20 +0200

    Add under exposure progress component

---
+++ src/app/views/main/pages/progress/under-exposure/under-exposure.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "SymUnderExposure",
+    templateUrl: "./under-exposure.component.html",
+    styleUrls: ["./under-exposure.component.scss"],
+})
+export class UnderExposureComponent implements OnInit {
+    constructor() {
+        // Initialize dependencies here
+    }
+
+    ngOnInit() {
+        // Use initialized dependencies
+    }
+}
