4d1077a3b1ad14fe43c2e340bae86fc47ce9208d
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-07 19:51:10 +0200

    Integrate dialogs in settings modal page

--- src/app/views/main/modals/settings/settings-container/settings-container.component.ts
+++ src/app/views/main/modals/settings/settings-container/settings-container.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ModalDialogParams } from "nativescript-angular/modal-dialog";
+import { DialogsService } from "~/app/views/common/dialogs.service";
 
 @Component({
     selector: "SymSettingsContainer",
@@ -7,9 +8,28 @@ import { ModalDialogParams } from "nativescript-angular/modal-dialog";
     styleUrls: ["./settings-container.component.scss"],
 })
 export class SettingsContainerComponent {
-    constructor(private params: ModalDialogParams) {}
+    constructor(
+        private params: ModalDialogParams,
+        private dialogsService: DialogsService
+    ) {}
 
     onClose() {
         this.params.closeCallback();
+    }
+
+    onUnlinkTap() {
+        this.dialogsService
+            .askConfirmationWithDestructiveAction(
+                "¿Desvincular dispositivo?",
+                "Salir",
+                "Volver",
+                "Si desvinculas este dispositivo perderás todo el progreso almacenado localmente (p. ej. contenido psicoeducativo visto) y tendrás que volver a configurar la app en caso de reinstalación"
+            )
+            .then((unlink) => {
+                if (unlink) {
+                    // TODO: Propagate this to somewhere else (a service maybe?)
+                    this.onClose();
+                }
+            });
     }
 }
