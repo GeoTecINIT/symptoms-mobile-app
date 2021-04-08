490d5cb6d729c87f2bdb14cfc21c860c16eefa89
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-08 18:38:36 +0200

    Add questions module and routes

---
+++ src/app/views/main/modals/questions/questions.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+
+import { QuestionsRoutingModule } from "./questions-routing.module";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { CommonComponentsModule } from "~/app/views/common/common-components.module";
+
+import { QuestionsModalComponent } from "./questions-modal.component";
+import { QuestionsContainerComponent } from "./questions-container/questions-container.component";
+
+@NgModule({
+    imports: [
+        QuestionsRoutingModule,
+        NativeScriptCommonModule,
+        CommonComponentsModule,
+    ],
+    declarations: [QuestionsModalComponent, QuestionsContainerComponent],
+    entryComponents: [QuestionsModalComponent],
+    schemas: [NO_ERRORS_SCHEMA],
+})
+export class QuestionsModule {}
