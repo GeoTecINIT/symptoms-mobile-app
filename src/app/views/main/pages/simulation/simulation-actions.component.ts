194de1b0b4ce28601a83afcb2c780322eb988b26
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-08 17:51:36 +0200

    Update simulation actions to enable modal access

--- src/app/views/main/pages/simulation/simulation-actions.component.ts
+++ src/app/views/main/pages/simulation/simulation-actions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ConfirmModalService } from "../../modals/confirm/confirm-modal.service";
 
 @Component({
     selector: "SymSimulationActions",
@@ -6,11 +7,17 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./simulation-actions.component.scss"],
 })
 export class SimulationActionsComponent implements OnInit {
-    constructor() {
+    constructor(private confirmModalService: ConfirmModalService) {
         // Initialize dependencies here
     }
 
     ngOnInit() {
         // Use initialized dependencies
+    }
+
+    onRelevantPlaceArrival() {
+        this.confirmModalService
+            .show()
+            .catch((e) => console.error("Could not show confirm modal:", e));
     }
 }
