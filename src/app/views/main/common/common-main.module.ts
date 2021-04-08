3b18fdf643a182f88dbd4764205f2efab7ffc97d
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-08 19:09:12 +0200

    Integrate feedback module inside common main module

--- src/app/views/main/common/common-main.module.ts
+++ src/app/views/main/common/common-main.module.ts
@@ -6,6 +6,7 @@ import { NotificationsModule } from "../modals/notifications/notifications.modul
 import { ContentViewModule } from "../modals/content-view/content-view.module";
 import { ConfirmModule } from "../modals/confirm/confirm.module";
 import { QuestionsModule } from "../modals/questions/questions.module";
+import { FeedbackModule } from "../modals/feedback/feedback.module";
 
 import { MainActionBarComponent } from "./main-action-bar/main-action-bar.component";
 
@@ -17,6 +18,7 @@ import { MainActionBarComponent } from "./main-action-bar/main-action-bar.compon
         ContentViewModule,
         ConfirmModule,
         QuestionsModule,
+        FeedbackModule,
     ],
     declarations: [MainActionBarComponent],
     exports: [MainActionBarComponent],
