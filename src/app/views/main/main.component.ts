bd98ea786bf09550e70dded21c82e27e9de5cd5c
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-12 13:29:29 +0200

    Migrate to an alternative navigation bar

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -7,12 +7,26 @@ import { Page } from "tns-core-modules/ui/page/page";
 
 import { MainViewService } from "./main-view.service";
 
+import {
+    BottomNavigationBar,
+    TabSelectedEventData,
+} from "nativescript-material-bottomnavigationbar";
+
+const navigationTabs = {
+    Progress: 0,
+    Content: 1,
+    Simulation: 2,
+};
+
 @Component({
     selector: "SymMain",
     templateUrl: "./main.component.html",
     styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
+    navigationTabs = navigationTabs;
+    selectedTab = navigationTabs.Progress;
+
     constructor(
         private routerExtension: RouterExtensions,
         private activeRoute: ActivatedRoute,
@@ -39,5 +53,14 @@ export class MainComponent implements OnInit {
                 { relativeTo: this.activeRoute }
             )
             .catch((e) => console.error("On MainComponent navigation:", e));
+    }
+
+    onNavigationBarLoaded(args: any) {
+        const navigationBar: BottomNavigationBar = args.object;
+        // Set here a badge if needed
+    }
+
+    onTabSelected(args: TabSelectedEventData) {
+        this.selectedTab = args.newIndex;
     }
 }
