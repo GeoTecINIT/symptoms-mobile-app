f7f0576f99afaa61a8d5254ce364f7f1b4c4bdaa
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-12 19:26:40 +0200

    Create a common primary button component

--- src/app/views/common/common-components.module.ts
+++ src/app/views/common/common-components.module.ts
@@ -6,6 +6,8 @@ import { CloseActionBarComponent } from "./close-action-bar/close-action-bar.com
 import { BackActionBarComponent } from "./back-action-bar/back-action-bar.component";
 import { SlideNavigationButtonComponent } from "./slide-navigation-button/slide-navigation-button.component";
 import { BasicActionBarComponent } from "./basic-action-bar/basic-action-bar.component";
+import { ButtonLinkComponent } from "./button-link/button-link.component";
+import { PrimaryButtonComponent } from "./primary-button/primary-button.component";
 
 @NgModule({
     imports: [NativeScriptCommonModule, NativeScriptRouterModule],
@@ -14,6 +16,8 @@ import { BasicActionBarComponent } from "./basic-action-bar/basic-action-bar.com
         BackActionBarComponent,
         SlideNavigationButtonComponent,
         BasicActionBarComponent,
+        ButtonLinkComponent,
+        PrimaryButtonComponent,
     ],
     schemas: [NO_ERRORS_SCHEMA],
     exports: [
@@ -21,6 +25,8 @@ import { BasicActionBarComponent } from "./basic-action-bar/basic-action-bar.com
         BackActionBarComponent,
         SlideNavigationButtonComponent,
         BasicActionBarComponent,
+        ButtonLinkComponent,
+        PrimaryButtonComponent,
     ],
 })
 export class CommonComponentsModule {}
