7c63ba9ca643925347a55b87710333a565aa6389
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-12 19:28:24 +0200

    Add dummy auth service

---
+++ src/app/views/auth.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from "@angular/core/testing";
+
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { AuthService } from "./auth.service";
+
+describe("AuthService", () => {
+    beforeEach(() =>
+        TestBed.configureTestingModule({ imports: [NativeScriptModule] })
+    );
+
+    it("should be created", () => {
+        const service: AuthService = TestBed.get(AuthService);
+        expect(service).toBeTruthy();
+    });
+});
