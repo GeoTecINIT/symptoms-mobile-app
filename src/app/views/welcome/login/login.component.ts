5df99eca00cbd76e2b89be74af3236e09774c48f
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-12 19:32:30 +0200

    Update login screen to be fully functional

--- src/app/views/welcome/login/login.component.ts
+++ src/app/views/welcome/login/login.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { InAppBrowserService } from "~/app/views/common/in-app-browser.service";
+import { AuthService } from "~/app/views/auth.service.ts";
+import { NavigationService } from "~/app/views/navigation.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: "SymLogin",
     templateUrl: "./login.component.html",
     styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent implements OnInit {
-    constructor(private inAppBrowserService: InAppBrowserService) {
-        // Initialize dependencies here
+export class LoginComponent {
+    canContinue = false;
+    authFailed = false;
+
+    private code: string;
+
+    constructor(
+        private authService: AuthService,
+        private navigationService: NavigationService,
+        private inAppBrowserService: InAppBrowserService,
+        private activeRoute: ActivatedRoute
+    ) {}
+
+    onCodeInputChange(code: string) {
+        this.authFailed = false;
+        this.code = code.trim();
+        this.canContinue = this.code !== "";
     }
 
-    ngOnInit() {
-        // Use initialized dependencies
+    onContinueTap() {
+        this.authService.login(this.code).then((success) => {
+            if (success) {
+                this.navigationService.navigate(
+                    ["../tutorial"],
+                    this.activeRoute,
+                    true
+                );
+            } else {
+                this.authFailed = true;
+            }
+        });
     }
 
     onOpenWebsiteTap() {
