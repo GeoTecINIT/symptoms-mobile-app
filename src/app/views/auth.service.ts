776733f5f88d38ffdcee1a6c99c6e1dece392939
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-13 13:32:56 +0200

    Update auth service to keep login state

--- src/app/views/auth.service.ts
+++ src/app/views/auth.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from "@angular/core";
+import { Observable, ReplaySubject } from "rxjs";
+import { getBoolean, setBoolean } from "tns-core-modules/application-settings";
+
+const LOGGED_IN_KEY = "AUTH_LOGGED_IN";
 
 @Injectable({
     providedIn: "root",
 })
 export class AuthService {
+    get loggedIn$(): Observable<boolean> {
+        return this.authSubject.asObservable();
+    }
+
+    private authSubject = new ReplaySubject<boolean>(1);
+
+    constructor() {
+        const loggedIn = getBoolean(LOGGED_IN_KEY, false);
+        this.authSubject.next(loggedIn);
+    }
+
     async login(authCode: string): Promise<boolean> {
+        setBoolean(LOGGED_IN_KEY, true);
+        this.authSubject.next(true);
+
         return true;
     }
 }
