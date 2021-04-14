e801ffafac2614b654665efa565ad10530570b33
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-14 20:31:17 +0200

    Migrate config files to NS7

--- reference.d.ts
+++ reference.d.ts
@@ -1,7 +1,7 @@
-/// <reference path="node_modules/tns-platform-declarations/android-29.d.ts" />
-/// <reference path="native_declarations/android-play-services-location.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
 
-/// <reference path="node_modules/tns-platform-declarations/ios.d.ts" />
+/// <reference path="native_declarations/android-play-services-location.d.ts" />
 
 declare namespace NodeJS {
     interface Global {
