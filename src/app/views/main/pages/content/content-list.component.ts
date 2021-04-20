1f8d5d20a4e3d79320a6ef06ea73dae9c5d7a3b6
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-20 20:18:18 +0200

    Update content list to use content list item component

--- src/app/views/main/pages/content/content-list.component.ts
+++ src/app/views/main/pages/content/content-list.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 
+import {
+    TreatmentContent,
+    TreatmentContentService,
+} from "~/app/views/treatment-content.service";
 import { ContentViewModalService } from "../../modals/content-view/content-view-modal.service";
+import { Observable } from "rxjs";
+import { take } from "rxjs/operators";
 
 @Component({
     selector: "SymContentList",
@@ -8,15 +14,34 @@ import { ContentViewModalService } from "../../modals/content-view/content-view-
     styleUrls: ["./content-list.component.scss"],
 })
 export class ContentListComponent implements OnInit {
-    constructor(private contentViewModalService: ContentViewModalService) {
-        // Initialize dependencies here
+    contents$: Observable<Array<TreatmentContent>>;
+
+    constructor(
+        private treatmentContentService: TreatmentContentService,
+        private contentViewModalService: ContentViewModalService
+    ) {
+        this.contents$ = this.treatmentContentService.contents$;
     }
 
     ngOnInit() {
         // Use initialized dependencies
     }
 
-    onOpenContent() {
-        this.contentViewModalService.showContent("introducción");
+    onListItemTap(args: any) {
+        this.fetchContentByIndex(args.index).then((content) => {
+            this.onOpenContent(content.id);
+        });
+    }
+
+    private async fetchContentByIndex(
+        index: number
+    ): Promise<TreatmentContent> {
+        const contents = await this.contents$.pipe(take(1)).toPromise();
+
+        return contents[index];
+    }
+
+    private onOpenContent(id: string) {
+        this.contentViewModalService.showContent(id);
     }
 }
