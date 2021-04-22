83ff036d4d808abd9708f51aedb52c90402aa1ec
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-22 20:54:03 +0200

    Temporally add line chart to progress screen for testing

--- src/app/views/main/pages/progress/progress-container.component.ts
+++ src/app/views/main/pages/progress/progress-container.component.ts
@@ -1,4 +1,39 @@
 import { Component, OnInit } from "@angular/core";
+import {
+    ChartData2D,
+    CuttingLines,
+    YAxisDataRange,
+} from "./common/charts/line-chart/line-chart.component";
+
+const TEST_DATA: Array<ChartData2D> = [
+    {
+        label: "Nivel de ansiedad",
+        values: [
+            { x: 0, y: 2 },
+            { x: 1, y: 2 },
+            { x: 2, y: 4 },
+            { x: 3, y: 7 },
+            { x: 4, y: 8 },
+            { x: 5, y: 7 },
+            { x: 6, y: 6 },
+            { x: 7, y: 5 },
+            { x: 8, y: 6 },
+            { x: 9, y: 5 },
+            { x: 10, y: 5 },
+            { x: 11, y: 3 },
+        ],
+    },
+];
+const Y_AXIS_DATA_RANGE: YAxisDataRange = {
+    min: 0,
+    max: 10,
+};
+
+const CUTTING_LINES: CuttingLines = [
+    { label: "Leve", value: 2 },
+    { label: "Moderada", value: 5 },
+    { label: "Alta", value: 8 },
+];
 
 @Component({
     selector: "SymProgressContainer",
@@ -6,6 +41,10 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./progress-container.component.scss"],
 })
 export class ProgressContainerComponent implements OnInit {
+    testData = TEST_DATA;
+    yAxisDataRange = Y_AXIS_DATA_RANGE;
+    cuttingLines = CUTTING_LINES;
+
     idle = true;
 
     constructor() {
