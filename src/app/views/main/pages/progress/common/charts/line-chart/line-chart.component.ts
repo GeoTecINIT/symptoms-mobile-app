c7f81660b30fd3ac9b5b008cb7416efa9ea60b26
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-22 20:53:33 +0200

    Basic styled line chart working

---
+++ src/app/views/main/pages/progress/common/charts/line-chart/line-chart.component.ts
@@ -0,0 +1,178 @@
+import { Component, Input } from "@angular/core";
+import { Color, EventData, Font } from "@nativescript/core";
+import { LineChart } from "@nativescript-community/ui-chart";
+import {
+    LineDataSet,
+    Mode,
+} from "@nativescript-community/ui-chart/data/LineDataSet";
+import { LineData } from "@nativescript-community/ui-chart/data/LineData";
+import { XAxisPosition } from "@nativescript-community/ui-chart/components/XAxis";
+import {
+    LimitLabelPosition,
+    LimitLine,
+} from "@nativescript-community/ui-chart/components/LimitLine";
+import {
+    LegendForm,
+    LegendHorizontalAlignment,
+} from "@nativescript-community/ui-chart/components/Legend";
+
+const CHART_BACKGROUND = "white";
+const X_ANIMATION_MILLIS = 500;
+const Y_ANIMATION_MILLIS = 1000;
+
+const AXIS_LABEL_TEXT_COLOR = "#828282";
+const AXIS_LINE_COLOR = "#E0E0E0";
+
+const CUTTING_LINE_TEXT_COLOR = "#1F525E";
+const CUTTING_LINE_COLOR = "#2DC38A";
+
+const TEXT_FONT_SIZE = 12;
+
+const COLOR_SCHEME = ["#3f2dc3", "#c3009f", "#fe006c", "#ff613a", "#ffa600"];
+const LINE_COLOR_ALPHA = 0.7;
+
+@Component({
+    selector: "SymLineChart",
+    templateUrl: "./line-chart.component.html",
+    styleUrls: ["./line-chart.component.scss"],
+})
+export class LineChartComponent {
+    @Input() data: Array<ChartData2D> = [];
+    @Input() yAxisDataRange?: YAxisDataRange;
+    @Input() cuttingLines: CuttingLines = [];
+
+    private chart: LineChart;
+    private textFont = Font.default.withFontSize(TEXT_FONT_SIZE);
+    private readonly colorScheme: Array<Color>;
+
+    constructor() {
+        this.colorScheme = COLOR_SCHEME.map((color) => {
+            const nsColor = new Color(color);
+
+            return new Color(
+                Math.round(LINE_COLOR_ALPHA * 255),
+                nsColor.r,
+                nsColor.g,
+                nsColor.b
+            );
+        });
+    }
+
+    onChartLoaded(event: EventData) {
+        this.chart = event.object as LineChart;
+
+        this.configureChart();
+        this.configureYAxis();
+        this.configureXAxis();
+        this.configureLegend();
+        this.addCuttingLines();
+
+        const sets = this.generateDataSets();
+
+        const ld = new LineData(sets);
+        this.chart.setData(ld);
+    }
+
+    private configureChart() {
+        this.chart.backgroundColor = CHART_BACKGROUND;
+        this.chart.setDrawGridBackground(false);
+
+        this.chart.animateXY(X_ANIMATION_MILLIS, Y_ANIMATION_MILLIS);
+
+        // disable dual axis (only use LEFT axis)
+        this.chart.getAxisRight().setEnabled(false);
+    }
+
+    private configureYAxis() {
+        const yAxis = this.chart.getAxisLeft();
+        yAxis.setDrawGridLines(false);
+        yAxis.setDrawAxisLine(false);
+        yAxis.setFont(this.textFont);
+        yAxis.setTextColor(AXIS_LABEL_TEXT_COLOR);
+        if (this.yAxisDataRange) {
+            const range = this.yAxisDataRange;
+            yAxis.setAxisMinValue(range.min - 1);
+            yAxis.setAxisMaxValue(range.max + 1);
+        }
+    }
+
+    private configureXAxis() {
+        const xAxis = this.chart.getXAxis();
+        xAxis.setPosition(XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisLineColor(AXIS_LINE_COLOR);
+        xAxis.setFont(this.textFont);
+        xAxis.setTextColor(AXIS_LABEL_TEXT_COLOR);
+    }
+
+    private configureLegend() {
+        if (this.data.length === 1) return;
+
+        const legend = this.chart.getLegend();
+        legend.setEnabled(true);
+        legend.setFont(this.textFont);
+        legend.setTextColor(AXIS_LABEL_TEXT_COLOR);
+        legend.setForm(LegendForm.LINE);
+        legend.setHorizontalAlignment(LegendHorizontalAlignment.RIGHT);
+        legend.setWordWrapEnabled(true);
+    }
+
+    private addCuttingLines() {
+        if (this.cuttingLines.length === 0) return;
+
+        const yAxis = this.chart.getAxisLeft();
+        yAxis.setDrawLimitLinesBehindData(false);
+
+        for (const line of this.cuttingLines) {
+            const limitLine = new LimitLine(line.value, line.label);
+            limitLine.setLabelPosition(LimitLabelPosition.LEFT_TOP);
+            limitLine.setLineColor(CUTTING_LINE_COLOR);
+            limitLine.setFont(this.textFont);
+            limitLine.setTextColor(CUTTING_LINE_TEXT_COLOR);
+            yAxis.addLimitLine(limitLine);
+        }
+    }
+
+    private generateDataSets(): Array<LineDataSet> {
+        const sets = [];
+
+        for (let i = 0; i < this.data.length; i++) {
+            const dataset = this.data[i];
+            const set = this.generateDataSet(i, dataset);
+            sets.push(set);
+        }
+
+        return sets;
+    }
+
+    private generateDataSet(index: number, dataSet: ChartData2D): LineDataSet {
+        const set = new LineDataSet(dataSet.values, dataSet.label, "x", "y");
+        set.setColor(this.colorScheme[index]);
+        set.setLineWidth(3);
+        set.setMode(Mode.CUBIC_BEZIER);
+
+        return set;
+    }
+}
+
+export interface ChartData2D {
+    label: string;
+    values: Array<ChartEntry2D>;
+}
+
+interface ChartEntry2D {
+    x: number;
+    y: number;
+}
+
+export type CuttingLines = Array<CuttingLine>;
+
+interface CuttingLine {
+    value: number;
+    label: string;
+}
+
+export interface YAxisDataRange {
+    min: number;
+    max: number;
+}
