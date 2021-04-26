75d25a1b6f6dfc9def379173c6b254537e51eaf1
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-26 19:50:51 +0200

    Update idle progress page to use title card with charts

--- src/app/views/main/pages/progress/idle-progress/idle-progress.component.ts
+++ src/app/views/main/pages/progress/idle-progress/idle-progress.component.ts
@@ -1,6 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { NavigationService } from "~/app/views/navigation.service";
 import { ActivatedRoute } from "@angular/router";
+import {
+    SESSION_DATA,
+    Y_AXIS_DATA_RANGE,
+    CUTTING_LINES,
+    AGGREGATE_DATA,
+} from "../pages/common";
+import { ChartData2D } from "../common/charts";
 
 @Component({
     selector: "SymIdleProgress",
@@ -8,6 +15,28 @@ import { ActivatedRoute } from "@angular/router";
     styleUrls: ["./idle-progress.component.scss"],
 })
 export class IdleProgressComponent {
+    @Input()
+    set hasData(hasData: boolean) {
+        this._hasData = hasData;
+        if (hasData) {
+            this.latestData = SESSION_DATA;
+            this.summaryData = AGGREGATE_DATA;
+        } else {
+            this.latestData = undefined;
+            this.summaryData = undefined;
+        }
+    }
+    get hasData(): boolean {
+        return this._hasData;
+    }
+
+    latestData: Array<ChartData2D>;
+    summaryData: Array<ChartData2D>;
+    yAxisDataRange = Y_AXIS_DATA_RANGE;
+    cuttingLines = CUTTING_LINES;
+
+    private _hasData = false;
+
     constructor(
         private navigationService: NavigationService,
         private activeRoute: ActivatedRoute
