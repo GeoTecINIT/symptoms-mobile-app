ba456c501939167d96b503efc5a7bc34a2d78dc4
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-26 17:06:52 +0200

    Attach section component to idle progress page

--- src/app/views/main/pages/progress/idle-progress/idle-progress.component.ts
+++ src/app/views/main/pages/progress/idle-progress/idle-progress.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { NavigationService } from "~/app/views/navigation.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: "SymIdleProgress",
     templateUrl: "./idle-progress.component.html",
     styleUrls: ["./idle-progress.component.scss"],
 })
-export class IdleProgressComponent implements OnInit {
-    constructor() {
-        // Initialize dependencies here
+export class IdleProgressComponent {
+    constructor(
+        private navigationService: NavigationService,
+        private activeRoute: ActivatedRoute
+    ) {}
+
+    onSeeRecordsTap() {
+        this.navigate("../records-list");
     }
 
-    ngOnInit() {
-        // Use initialized dependencies
+    onSeeAggregatesTap() {
+        this.navigate("../aggregate-list");
+    }
+
+    private navigate(route: string) {
+        this.navigationService.navigate([route], this.activeRoute);
     }
 }
