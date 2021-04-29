0d2a2950f363d473dad9df2759ddda66cf017fbc
Alberto Gonzalez <algonzal@uji.es>
2021-04-29 20:39:58 +0200

    Create feedback text option

--- src/app/views/main/modals/feedback/feedback.module.ts
+++ src/app/views/main/modals/feedback/feedback.module.ts
@@ -6,14 +6,23 @@ import { CommonComponentsModule } from "~/app/views/common/common-components.mod
 
 import { FeedbackModalComponent } from "./feedback-modal.component";
 import { FeedbackContainerComponent } from "./feedback-container/feedback-container.component";
+import { CommonModalsModule } from "../common/common-modals.module";
+import { FeedbackButtonOptionComponent } from "./options/feedback-button-option/feedback-button-option.component";
+import { FeedbackTextOptionComponent } from "./options/feedback-text-option/feedback-text-option.component";
 
 @NgModule({
     imports: [
         FeedbackRoutingModule,
         NativeScriptCommonModule,
         CommonComponentsModule,
+        CommonModalsModule,
     ],
-    declarations: [FeedbackModalComponent, FeedbackContainerComponent],
+    declarations: [
+        FeedbackModalComponent,
+        FeedbackContainerComponent,
+        FeedbackButtonOptionComponent,
+        FeedbackTextOptionComponent,
+    ],
     entryComponents: [FeedbackModalComponent],
     schemas: [NO_ERRORS_SCHEMA],
 })
---
+++ src/app/views/main/modals/feedback/options/feedback-text-option/feedback-text-option.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { EventData, TextField } from "@nativescript/core";
+
+@Component({
+    selector: "SymFeedbackTextOption",
+    templateUrl: "./feedback-text-option.component.html",
+    styleUrls: ["./feedback-text-option.component.scss"],
+})
+export class FeedbackTextOptionComponent {
+    @Input() hint = "";
+    @Output() answerProvided = new EventEmitter<string>();
+    @Input() helpText = "";
+    answer: string;
+    focus = false;
+
+    onConfirmTap() {
+        if (this.answer.trim() === "") return;
+        this.answerProvided.emit(this.answer);
+    }
+
+    onFieldFocus() {
+        this.focus = true;
+    }
+
+    onFieldBlur() {
+        this.focus = false;
+    }
+
+    onFieldTextChange(args: EventData) {
+        const textField = args.object as TextField;
+        this.answer = textField.text;
+    }
+}
