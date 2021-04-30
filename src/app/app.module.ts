f109bc5c43d3bccca3808dc6fb8526dc21abc8f0
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-30 17:58:37 +0200

    Add animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "@nativescript/angular";
+import {
+    NativeScriptAnimationsModule,
+    NativeScriptModule,
+} from "@nativescript/angular";
 import { NativeScriptLocalizeModule } from "@nativescript/localize/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -7,7 +10,12 @@ import { AppComponent } from "./app.component";
 
 @NgModule({
     bootstrap: [AppComponent],
-    imports: [NativeScriptModule, NativeScriptLocalizeModule, AppRoutingModule],
+    imports: [
+        NativeScriptModule,
+        NativeScriptAnimationsModule,
+        NativeScriptLocalizeModule,
+        AppRoutingModule,
+    ],
     declarations: [AppComponent],
     schemas: [NO_ERRORS_SCHEMA],
 })
