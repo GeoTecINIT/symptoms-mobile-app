a01c3abc1c2bc85f71d1d590073d70ec96b1df78
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-30 17:59:08 +0200

    Finalize feedback modal screen, adding optional confirm screen

--- src/app/views/main/modals/feedback/feedback-container/feedback-container.component.ts
+++ src/app/views/main/modals/feedback/feedback-container/feedback-container.component.ts
@@ -1,19 +1,48 @@
 import { Component } from "@angular/core";
 import { ModalDialogParams } from "@nativescript/angular";
+import { animate, style, transition, trigger } from "@angular/animations";
+import { FeedbackModalOptions } from "~/app/views/main/modals/feedback/options";
 
 @Component({
     selector: "SymFeedbackContainer",
     templateUrl: "./feedback-container.component.html",
     styleUrls: ["./feedback-container.component.scss"],
+    animations: [
+        trigger("slideUp", [
+            transition(":enter", [
+                style({ transform: "translateY(600%)" }),
+                animate(
+                    "300ms ease-in-out",
+                    style({ transform: "translateY(0)" })
+                ),
+            ]),
+        ]),
+    ],
 })
 export class FeedbackContainerComponent {
-    constructor(private params: ModalDialogParams) {}
+    options: FeedbackModalOptions;
+    answer: string;
+
+    get showConfirmScreen(): boolean {
+        return this.hasConfirmScreen && !!this.answer;
+    }
+
+    get hasConfirmScreen(): boolean {
+        return !!this.options.confirmScreen;
+    }
+
+    constructor(private params: ModalDialogParams) {
+        this.options = params.context as FeedbackModalOptions;
+    }
 
     onClose() {
-        this.params.closeCallback(undefined);
+        this.params.closeCallback(this.answer);
     }
 
     onAnswer(answer: string) {
-        this.params.closeCallback(answer);
+        this.answer = answer;
+        if (!this.hasConfirmScreen) {
+            this.onClose();
+        }
     }
 }
