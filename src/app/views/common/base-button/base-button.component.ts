be4d53f9cf70350c2ac1dff46d2a4d30034db7fc
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-30 20:35:27 +0200

    Create new base button with common primary btn config

---
+++ src/app/views/common/base-button/base-button.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+export type ButtonHAlignment = "left" | "right" | "center" | "stretch";
+export type ButtonSize = "lg" | "md" | "sm";
+export type ButtonVariant = "primary" | "secondary" | "confirm";
+
+@Component({
+    selector: "SymBaseButton",
+    templateUrl: "./base-button.component.html",
+    styleUrls: ["./base-button.component.scss"],
+})
+export class BaseButtonComponent {
+    @Input() variant: ButtonVariant = "primary";
+    @Input() text = "";
+    @Input() flat = false;
+    @Input() margin = 12;
+    @Input() horizontalAlignment: ButtonHAlignment = "stretch";
+    @Input() size: ButtonSize = "md";
+    @Input() enabled = true;
+
+    @Output() tap = new EventEmitter();
+
+    get primary(): boolean {
+        return this.variant === "primary";
+    }
+
+    get secondary(): boolean {
+        return this.variant === "secondary";
+    }
+
+    get confirm(): boolean {
+        return this.variant === "confirm";
+    }
+
+    onTap() {
+        this.tap.emit();
+    }
+}
