21eb03cd9db6ac88bd27edf8aa0dd6a4489a3344
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-04-30 20:39:02 +0200

    Update questions modal screen with demo question step

--- src/app/views/main/modals/questions/questions-container/questions-container.component.ts
+++ src/app/views/main/modals/questions/questions-container/questions-container.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ModalDialogParams } from "@nativescript/angular";
+import { QuestionType } from "~/app/views/main/modals/questions/options";
 
 @Component({
     selector: "SymQuestionsContainer",
@@ -7,6 +8,39 @@ import { ModalDialogParams } from "@nativescript/angular";
     styleUrls: ["./questions-container.component.scss"],
 })
 export class QuestionsContainerComponent {
+    questionStep = 0;
+
+    questions: Array<QuestionType> = [
+        {
+            title: "De 0 a 10, ¿cómo puntuarías tu nivel de ansiedad actual?",
+            type: "range",
+            from: 0,
+            to: 10,
+        },
+        {
+            title:
+                "De 0 a 10, ¿cómo puntuarías las ganas que tienes de salir de aquí?",
+            type: "range",
+            from: 0,
+            to: 10,
+        },
+        {
+            title:
+                "De 0 a 10, ¿cómo puntuarías tu grado de creencia en pensamientos?",
+            type: "range",
+            from: 0,
+            to: 10,
+        },
+    ];
+
+    get ongoingQuestion(): QuestionType {
+        return this.questions[this.questionStep];
+    }
+
+    get questionsAmount(): number {
+        return this.questions.length;
+    }
+
     constructor(private params: ModalDialogParams) {}
 
     onClose() {
