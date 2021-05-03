7839be40bdd4166b65a341b08b01d3c2dbc99a35
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-03 13:52:12 +0200

    Update simulation actions component to use the new questions modal API

--- src/app/views/main/pages/simulation/simulation-actions.component.ts
+++ src/app/views/main/pages/simulation/simulation-actions.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ConfirmModalService } from "../../modals/confirm";
-import { QuestionsModalService } from "../../modals/questions/questions-modal.service";
+import { QuestionsModalService } from "../../modals/questions";
 import { FeedbackModalService } from "../../modals/feedback";
 
 @Component({
@@ -41,7 +41,37 @@ export class SimulationActionsComponent implements OnInit {
 
     onWantsToAnswerQuestions() {
         this.questionsModalService
-            .deliverQuestions()
+            .deliverQuestions({
+                title: "¿Cómo te encuentras?",
+                body: {
+                    iconCode: "\ue94c",
+                    text:
+                        "Por favor, responde con sinceridad. Al hacerlo me ayudas a saber mejor cómo te encuentras",
+                },
+                questions: [
+                    {
+                        title:
+                            "De 0 a 10, ¿cómo puntuarías tu nivel de ansiedad actual?",
+                        type: "range",
+                        from: 0,
+                        to: 10,
+                    },
+                    {
+                        title:
+                            "De 0 a 10, ¿cómo puntuarías las ganas que tienes de salir de aquí?",
+                        type: "range",
+                        from: 0,
+                        to: 10,
+                    },
+                    {
+                        title:
+                            "De 0 a 10, ¿cómo puntuarías tu grado de creencia en pensamientos?",
+                        type: "range",
+                        from: 0,
+                        to: 10,
+                    },
+                ],
+            })
             .then((answers) => console.log("Got answers:", answers))
             .catch((e) => console.error("Could not deliver questions:", e));
     }
