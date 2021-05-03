42b2ca4cdcfc8e7ed8061f04588e42baf11e1b9a
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-03 13:49:13 +0200

    Define a data-driven API to configure the questions modal

--- src/app/views/main/modals/questions/options.ts
+++ src/app/views/main/modals/questions/options.ts
@@ -1,3 +1,13 @@
+export interface QuestionsModalOptions {
+    title: string;
+    body: {
+        emoji?: string;
+        iconCode?: string;
+        text: string;
+    };
+    questions: Array<QuestionType>;
+}
+
 export type QuestionType = RangeQuestion; // Append other question types here
 
 interface RangeQuestion extends Question {
