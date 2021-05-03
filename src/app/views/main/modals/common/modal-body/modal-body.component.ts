1da99829ec963e14c817a817fa48e443fdb702ef
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-03 13:37:01 +0200

    Update modal body to allow multi step workflows

--- src/app/views/main/modals/common/modal-body/modal-body.component.ts
+++ src/app/views/main/modals/common/modal-body/modal-body.component.ts
@@ -9,4 +9,7 @@ export class ModalBodyComponent {
     @Input() emoji: string;
     @Input() iconCode: string;
     @Input() bodyText = "";
+
+    @Input() currentStep: number;
+    @Input() stepAmount: number;
 }
