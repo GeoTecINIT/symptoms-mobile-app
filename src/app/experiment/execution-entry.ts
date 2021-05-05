255ca0cb9409e3442f9334bcfd1a22572f28189f
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-05 12:13:35 +0200

    Remove scheduler experiment code

--- src/app/experiment/execution-entry.ts
+++
@@ -1,8 +0,0 @@
-import { ExperimentTask } from "./experiment-tasks";
-
-export interface ExecutionEntry {
-    planning_timestamp: number;
-    exec_timestamp: number;
-    battery: number;
-    task: ExperimentTask;
-}
