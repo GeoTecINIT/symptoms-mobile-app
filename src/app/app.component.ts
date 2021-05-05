209fa1cf027c3f5da66225e7a1eb34f3894886db
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-05 12:14:17 +0200

    Remove unneeded external event handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { isAndroid } from "@nativescript/core";
-
-import { externalEventHandler } from "./core/events/external-event-handler";
 
 @Component({
     selector: "SymApp",
@@ -13,13 +10,6 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        // TODO: Discuss. Would it be interesting to have a mechanism
-        // where things to be done at startup time could be registered?
-        if (isAndroid) {
-            externalEventHandler.init();
-            // setupNotificationChannels(androidApp.context);
-            // const alarmScheduler = new AndroidAlarmScheduler();
-            // alarmScheduler.setup();
-        }
+        // Use dependencies here
     }
 }
