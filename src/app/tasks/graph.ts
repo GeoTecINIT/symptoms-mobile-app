611a75ce03e71e0f6895fc5b8494a3a370c0bb1c
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-05 12:17:59 +0200

    Cleanup demo and scheduler experiments tasks and related graph

--- src/app/tasks/graph.ts
+++ src/app/tasks/graph.ts
@@ -2,32 +2,14 @@ import {
     TaskGraph,
     EventListenerGenerator,
     RunnableTaskDescriptor,
-} from "nativescript-task-dispatcher/tasks/graph";
+} from "@geotecinit/emai-framework/tasks/graph";
 
 class DemoTaskGraph implements TaskGraph {
     async describe(
         on: EventListenerGenerator,
         run: RunnableTaskDescriptor
     ): Promise<void> {
-        on(
-            "startEvent",
-            run("logDummyTaskExecutionStart")
-                .every(1, "minutes")
-                .cancelOn("stopEvent")
-        );
-        on(
-            "startEvent",
-            run("logGPSTaskExecutionStart")
-                .every(1, "minutes")
-                .cancelOn("stopEvent")
-        );
-
-        on("dummyTaskExecutionStartLogged", run("acquireBatteryLevel"));
-
-        on("gpsTaskExecutionStartLogged", run("acquireGeolocation"));
-        on("geolocationAcquired", run("acquireBatteryLevel"));
-
-        on("batteryLevelAcquired", run("logTaskExecutionEnd"));
+        // TODO: Add tasks
     }
 }
 
