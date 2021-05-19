598145c57265f2361dba579ccee25a7526c4b62e
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-19 18:15:51 +0200

    Create a task to concentrate multiple events as one

---
+++ src/app/tasks/utility/event-converter.ts
@@ -0,0 +1,25 @@
+import {
+    DispatchableEvent,
+    Task,
+    TaskOutcome,
+    TaskParams,
+} from "@geotecinit/emai-framework/tasks";
+
+export class EventConverterTask extends Task {
+    constructor(outputEvent: string) {
+        super(`emit${titelize(outputEvent)}Event`, {
+            outputEventNames: [outputEvent],
+        });
+    }
+
+    protected async onRun(
+        taskParams: TaskParams,
+        invocationEvent: DispatchableEvent
+    ): Promise<TaskOutcome> {
+        return { result: invocationEvent.data };
+    }
+}
+
+function titelize(str: string) {
+    return `${str.slice(0, 1).toUpperCase()}${str.slice(1)}`;
+}
