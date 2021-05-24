fe9ed150f2a7214636b1871c072564cba64d9aad
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-24 17:26:53 +0200

    Update exposure change record to include the exposure place

--- src/app/tasks/exposure/change-record.ts
+++ src/app/tasks/exposure/change-record.ts
@@ -1,11 +1,13 @@
 import { Change, Record } from "@geotecinit/emai-framework/entities";
 import { EmotionValue } from "~/app/core/persistence/exposures";
+import { AreaOfInterest } from "@geotecinit/emai-framework/entities/aois";
 
 export class ExposureChange extends Record {
     constructor(
         change: Change,
         timestamp: Date,
-        public emotionValues: Array<EmotionValue>
+        public emotionValues: Array<EmotionValue>,
+        public aoi: AreaOfInterest
     ) {
         super("exposure-change", timestamp, change);
     }
