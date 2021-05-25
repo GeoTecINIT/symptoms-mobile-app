7ed69a265d20534288b298fcc5ff55bd3091f560
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-25 16:27:35 +0200

    Add notification tap and discard handlers

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,6 +1,11 @@
 import { Task } from "@geotecinit/emai-framework/tasks";
 
 import { utilityTasks } from "./utility";
+import { notificationTasks } from "./notifications";
 import { exposureTasks } from "./exposure";
 
-export const appTasks: Array<Task> = [...utilityTasks, ...exposureTasks];
+export const appTasks: Array<Task> = [
+    ...utilityTasks,
+    ...notificationTasks,
+    ...exposureTasks,
+];
