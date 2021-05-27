6e89d114c6276e13fcc229e607e6769e6985f906
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-27 21:55:39 +0200

    Add a task to check exposure finalization dropout condition

---
+++ src/app/tasks/exposure/finalization-dropout-checker.ts
@@ -0,0 +1,33 @@
+import {
+    DispatchableEvent,
+    TaskOutcome,
+    TaskParams,
+    TraceableTask,
+} from "@geotecinit/emai-framework/tasks";
+import { ExposureChange } from "~/app/tasks/exposure";
+
+const EXPOSURE_DROPPED_OUT = "exposureDroppedOut";
+const EXPOSURE_NOT_DROPPED_OUT = "exposureNotDroppedOut";
+
+export class ExposureFinalizationDropoutChecker extends TraceableTask {
+    constructor() {
+        super("checkIfExposureWasDroppedOut", {
+            outputEventNames: [EXPOSURE_DROPPED_OUT, EXPOSURE_NOT_DROPPED_OUT],
+        });
+    }
+
+    protected async onTracedRun(
+        taskParams: TaskParams,
+        invocationEvent: DispatchableEvent
+    ): Promise<TaskOutcome> {
+        const exposureChange = invocationEvent.data as ExposureChange;
+        if (exposureChange.successful) {
+            return {
+                eventName: EXPOSURE_NOT_DROPPED_OUT,
+                result: exposureChange,
+            };
+        } else {
+            return { eventName: EXPOSURE_DROPPED_OUT, result: exposureChange };
+        }
+    }
+}
--- src/app/tasks/exposure/index.ts
+++ src/app/tasks/exposure/index.ts
@@ -4,6 +4,7 @@ import { ProcessExposureAnswers } from "./process-answers";
 import { EvaluateExposureTask } from "./evaluate-exposure";
 import { EvaluateExposureExtensionTask } from "./evaluate-exposure-ext";
 import { FinishExposureTask } from "./finish-exposure";
+import { ExposureFinalizationDropoutChecker } from "./finalization-dropout-checker";
 import { ExposureStatusChecker } from "./escapes/exposure-status-checker";
 import { ExposureLeaveChecker } from "./escapes/exposure-leave-checker";
 import { ExposureReturnChecker } from "./escapes/exposure-return-checker";
@@ -18,6 +19,7 @@ export const exposureTasks: Array<Task> = [
     new EvaluateExposureTask(),
     new EvaluateExposureExtensionTask(),
     new FinishExposureTask(),
+    new ExposureFinalizationDropoutChecker(),
     // --> Escapes
     new ExposureStatusChecker(),
     new ExposureLeaveChecker(),
