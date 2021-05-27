9c346d41d287f61f8352d6cda1a55ccaafb08de0
Miguel <al341802@uji.es>
2021-05-27 15:14:22 +0200

    Remove notification event records and task handlers

--- src/app/tasks/notifications/index.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from "@geotecinit/emai-framework/tasks";
-import { NotificationTapHandlerTask } from "~/app/tasks/notifications/notification-tap-handler";
-import { NotificationDiscardHandlerTask } from "~/app/tasks/notifications/notification-discard-handler";
-
-export const notificationTasks: Array<Task> = [
-    new NotificationTapHandlerTask(),
-    new NotificationDiscardHandlerTask(),
-];
