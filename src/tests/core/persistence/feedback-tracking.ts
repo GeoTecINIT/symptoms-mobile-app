a20133a4a0b52527d89c2f438403ca4f58dba87a
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-05-27 21:55:18 +0200

    Update feedback tracking tests

--- src/tests/core/persistence/feedback-tracking.ts
+++ src/tests/core/persistence/feedback-tracking.ts
@@ -1,25 +1,24 @@
 import { feedbackTracking } from "~/app/core/persistence/feedback-tracking";
-import { PatientFeedback } from "~/app/core/modals/feedback";
 
 describe("Feedback tracking strore", () => {
     const store = feedbackTracking;
-    const exampleFeedback = new PatientFeedback("f1", "Feedback?", "Feedback");
+    const exampleFeedbackId = "f1";
 
     it("allows to track a feedback as acquired", async () => {
-        await store.track(exampleFeedback);
-        const count = await store.getCount(exampleFeedback);
+        await store.track(exampleFeedbackId);
+        const count = await store.getCount(exampleFeedbackId);
         expect(count).toBe(1);
     });
 
     it("increments the count of an already tracked feedback", async () => {
-        await store.track(exampleFeedback);
-        await store.track(exampleFeedback);
-        const count = await store.getCount(exampleFeedback);
+        await store.track(exampleFeedbackId);
+        await store.track(exampleFeedbackId);
+        const count = await store.getCount(exampleFeedbackId);
         expect(count).toBe(2);
     });
 
     it("returns a count of 0 when feedback has not been tracked yet", async () => {
-        const count = await store.getCount(exampleFeedback);
+        const count = await store.getCount(exampleFeedbackId);
         expect(count).toBe(0);
     });
 
