f051935993aaa5a530a6f6f40ca5d8aa043545fb
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-06-02 17:30:56 +0200

    Update patient data service with conditional query support

--- src/app/views/patient-data.service.ts
+++ src/app/views/patient-data.service.ts
@@ -11,11 +11,17 @@ const INT_MAX_VALUE = Math.pow(2, 31) - 1;
     providedIn: "root",
 })
 export class PatientDataService {
-    getLastByRecordType<T extends Record>(recordType: string): Observable<T> {
+    getLastByRecordType<T extends Record>(
+        recordType: string,
+        conditions: Array<QueryCondition> = []
+    ): Observable<T> {
         return recordsStore.list(INT_MAX_VALUE).pipe(
             map((records) => {
                 for (const record of records) {
-                    if (record.type === recordType) {
+                    if (
+                        record.type === recordType &&
+                        meetsConditions(record, conditions)
+                    ) {
                         return record as T;
                     }
                 }
@@ -24,4 +30,37 @@ export class PatientDataService {
             })
         );
     }
+}
+
+function meetsConditions(
+    record: any,
+    conditions: Array<QueryCondition>
+): boolean {
+    for (const condition of conditions) {
+        const propertyValue = record[condition.property];
+        if (propertyValue === undefined) {
+            return false;
+        }
+        switch (condition.comparison) {
+            case "=":
+                if (propertyValue !== condition.value) {
+                    return false;
+                } else {
+                    console.log("Meets condition!", condition);
+                    continue;
+                }
+            default:
+                throw new Error(
+                    `Unknown condition comparison: ${condition.comparison}`
+                );
+        }
+    }
+
+    return true;
+}
+
+export interface QueryCondition {
+    property: string;
+    comparison: "=";
+    value: any;
 }
