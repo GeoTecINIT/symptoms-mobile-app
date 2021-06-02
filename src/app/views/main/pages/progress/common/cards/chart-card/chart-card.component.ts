0bc046b7a647f3cf01f81510269e732cfe2a5802
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-06-02 17:29:58 +0200

    Add new chart card component

---
+++ src/app/views/main/pages/progress/common/cards/chart-card/chart-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from "@angular/core";
+import { Record } from "@geotecinit/emai-framework/entities";
+import { ChartDescription } from "~/app/core/charts/chart-description";
+import { transformIntoChartDescription } from "~/app/core/charts/data-transformer";
+
+export type ChartType = "line" | "bar";
+
+@Component({
+    selector: "SymChartCard",
+    templateUrl: "./chart-card.component.html",
+    styleUrls: ["./chart-card.component.scss"],
+})
+export class ChartCardComponent {
+    @Input()
+    set record(record: Record) {
+        this.chartDescription = transformIntoChartDescription(record);
+    }
+    @Input() chartType: ChartType;
+
+    chartDescription: ChartDescription;
+}
