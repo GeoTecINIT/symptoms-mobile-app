32bc7a59023de846d42f5269c47fd5c6fc030e6e
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-06-03 19:48:38 +0200

    Add two new chart data transformers for aggregates

--- src/app/core/charts/data-transformer.ts
+++ src/app/core/charts/data-transformer.ts
@@ -4,9 +4,14 @@ import { RecordType } from "~/app/core/record-type";
 import { ExposureChange } from "~/app/tasks/exposure";
 import { formatAsDate } from "~/app/core/utils/time";
 import {
+    ChartEntry2D,
     CuttingLines,
     YAxisDataRange,
 } from "~/app/views/main/pages/progress/common/charts";
+import {
+    ExposureAggregate,
+    ExposurePlaceAggregate,
+} from "~/app/tasks/visualizations";
 
 export const ANXIETY_LEVEL_RANGE: YAxisDataRange = {
     min: 0,
@@ -24,8 +29,12 @@ export function transformIntoChartDescription(
 ): ChartDescription {
     switch (record.type) {
         case RecordType.ExposureChange:
-            return transformExposureChangeIntoChartDescription(
-                record as ExposureChange
+            return transformExposureChange(record as ExposureChange);
+        case RecordType.ExposureAggregate:
+            return transformExposureAggregate(record as ExposureAggregate);
+        case RecordType.ExposurePlaceAggregate:
+            return transformExposurePlaceAggregate(
+                record as ExposurePlaceAggregate
             );
         default:
             throw new Error(
@@ -34,7 +43,7 @@ export function transformIntoChartDescription(
     }
 }
 
-function transformExposureChangeIntoChartDescription(
+function transformExposureChange(
     exposureChange: ExposureChange
 ): ChartDescription {
     return {
@@ -50,6 +59,65 @@ function transformExposureChangeIntoChartDescription(
                 {
                     label: "Nivel de ansiedad",
                     values: exposureChange.emotionValues.map(
+                        (emotionValue) => ({
+                            x: emotionValue.timestamp,
+                            y: emotionValue.value,
+                        })
+                    ),
+                },
+            ],
+        },
+    };
+}
+
+function transformExposureAggregate(
+    exposureAggregate: ExposureAggregate
+): ChartDescription {
+    const placesEntries = new Map<string, Array<ChartEntry2D>>();
+    for (const aggregateData of exposureAggregate.data) {
+        const placeName = aggregateData.place.name;
+
+        const entry: ChartEntry2D = {
+            x: aggregateData.timestamp,
+            y: aggregateData.value,
+        };
+
+        const entries = placesEntries.has(placeName)
+            ? [...placesEntries.get(placeName), entry]
+            : [entry];
+
+        placesEntries.set(placeName, entries);
+    }
+
+    return {
+        iconCode: "\ue26b",
+        title: "En todos los lugares",
+        subtitle: "Nivel de ansidedad medio",
+        chart: {
+            yAxisDataRange: ANXIETY_LEVEL_RANGE,
+            cuttingLines: ANXIETY_THRESHOLDS,
+            data: [...placesEntries.keys()].map((placeName) => ({
+                label: placeName,
+                values: placesEntries.get(placeName),
+            })),
+        },
+    };
+}
+
+function transformExposurePlaceAggregate(
+    exposurePlaceAggregate: ExposurePlaceAggregate
+): ChartDescription {
+    return {
+        iconCode: "\ue26b",
+        title: `En ${exposurePlaceAggregate.placeName}`,
+        subtitle: "Nivel de ansidedad medio",
+        chart: {
+            yAxisDataRange: ANXIETY_LEVEL_RANGE,
+            cuttingLines: ANXIETY_THRESHOLDS,
+            data: [
+                {
+                    label: "Nivel de ansiedad medio",
+                    values: exposurePlaceAggregate.emotionValues.map(
                         (emotionValue) => ({
                             x: emotionValue.timestamp,
                             y: emotionValue.value,
