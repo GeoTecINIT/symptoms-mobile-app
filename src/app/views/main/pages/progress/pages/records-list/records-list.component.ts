1e0fc5e708c6cc6a4b499680062b4680030172d2
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-06-03 19:51:04 +0200

    Update records list to show real records list

--- src/app/views/main/pages/progress/pages/records-list/records-list.component.ts
+++ src/app/views/main/pages/progress/pages/records-list/records-list.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from "@angular/core";
-import { FAKE_RECORDS_LIST } from "./data";
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { Record } from "@geotecinit/emai-framework/entities";
+import { PatientDataService } from "~/app/views/patient-data.service";
+import { RecordType } from "~/app/core/record-type";
+import { Change } from "@geotecinit/emai-framework/internal/providers";
 
 @Component({
     selector: "SymRecordsList",
     templateUrl: "./records-list.component.html",
     styleUrls: ["./records-list.component.scss"],
 })
-export class RecordsListComponent implements OnInit {
-    data = FAKE_RECORDS_LIST;
+export class RecordsListComponent {
+    records$: Observable<Array<Record>>;
 
-    constructor() {
-        // Initialize dependencies here
-    }
-
-    ngOnInit() {
-        // Use initialized dependencies
+    constructor(private patientDataService: PatientDataService) {
+        this.records$ = this.patientDataService.observeRecordsByType(
+            RecordType.ExposureChange,
+            [
+                { property: "change", comparison: "=", value: Change.END },
+                { property: "successful", comparison: "=", value: true },
+            ]
+        );
     }
 }
