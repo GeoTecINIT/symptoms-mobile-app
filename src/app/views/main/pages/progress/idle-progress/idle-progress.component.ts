9b5e5b823497e01c1a57baf521d045ace9efc913
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-06-03 19:49:45 +0200

    Update idle progress view to show general evolution aggregate

--- src/app/views/main/pages/progress/idle-progress/idle-progress.component.ts
+++ src/app/views/main/pages/progress/idle-progress/idle-progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
 import { NavigationService } from "~/app/views/navigation.service";
 import { PatientDataService } from "~/app/views/patient-data.service";
 import { ActivatedRoute } from "@angular/router";
@@ -7,13 +7,8 @@ import { Observable } from "rxjs";
 import { Record } from "@geotecinit/emai-framework/entities";
 import { RecordType } from "~/app/core/record-type";
 import { Change } from "@geotecinit/emai-framework/internal/providers";
-
-import {
-    Y_AXIS_DATA_RANGE,
-    CUTTING_LINES,
-    AGGREGATE_DATA,
-} from "../pages/common";
-import { ChartData2D } from "../common/charts";
+import { share } from "rxjs/operators";
+import { map } from "rxjs/internal/operators";
 
 @Component({
     selector: "SymIdleProgress",
@@ -21,37 +16,29 @@ import { ChartData2D } from "../common/charts";
     styleUrls: ["./idle-progress.component.scss"],
 })
 export class IdleProgressComponent {
-    @Input()
-    set hasData(hasData: boolean) {
-        this._hasData = hasData;
-        if (hasData) {
-            this.summaryData = AGGREGATE_DATA;
-        } else {
-            this.summaryData = undefined;
-        }
-    }
-    get hasData(): boolean {
-        return this._hasData;
-    }
-
     latestData$: Observable<Record>;
-    summaryData: Array<ChartData2D>;
-    yAxisDataRange = Y_AXIS_DATA_RANGE;
-    cuttingLines = CUTTING_LINES;
-
-    private _hasData = false;
+    hasLatestData$: Observable<boolean>;
+    summaryData$: Observable<Record>;
+    hasSummaryData$: Observable<boolean>;
 
     constructor(
         private navigationService: NavigationService,
         private patientDataService: PatientDataService,
         private activeRoute: ActivatedRoute
     ) {
-        this.latestData$ = this.patientDataService.observeLastByRecordType(
-            RecordType.ExposureChange,
-            [
+        this.latestData$ = this.patientDataService
+            .observeLastByRecordType(RecordType.ExposureChange, [
                 { property: "change", comparison: "=", value: Change.END },
                 { property: "successful", comparison: "=", value: true },
-            ]
+            ])
+            .pipe(share());
+        this.hasLatestData$ = this.latestData$.pipe(map((record) => !!record));
+
+        this.summaryData$ = this.patientDataService
+            .observeLastByRecordType(RecordType.ExposureAggregate)
+            .pipe(share());
+        this.hasSummaryData$ = this.summaryData$.pipe(
+            map((summary) => !!summary)
         );
     }
 
