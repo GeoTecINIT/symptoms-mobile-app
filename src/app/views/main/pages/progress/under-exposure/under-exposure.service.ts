4ec03befb79d75f817250570ca10c214b0df49a5
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-06-09 13:57:02 +0200

    Update under exposure service with therapist phone call support

--- src/app/views/main/pages/progress/under-exposure/under-exposure.service.ts
+++ src/app/views/main/pages/progress/under-exposure/under-exposure.service.ts
@@ -3,6 +3,8 @@ import { Observable, of, merge } from "rxjs";
 import { Exposure, exposures } from "~/app/core/persistence/exposures";
 import { share, switchMap } from "rxjs/operators";
 import { map } from "rxjs/internal/operators";
+import { requestCallPermission, dial } from "nativescript-phone";
+import { getLogger, Logger } from "~/app/core/utils/logger";
 
 const DANGER_THRESHOLD = 8;
 
@@ -18,10 +20,14 @@ export class UnderExposureService {
         return this._inDanger$;
     }
 
+    private readonly therapistPhone: string;
+
     private readonly _ongoingExposure$: Observable<Exposure>;
     private readonly _inDanger$: Observable<boolean>;
+    private logger: Logger;
 
     constructor() {
+        this.logger = getLogger("UnderExposureService");
         this._ongoingExposure$ = merge(of([]), exposures.changes).pipe(
             switchMap(() => exposures.getLastUnfinished()),
             share()
@@ -29,6 +35,35 @@ export class UnderExposureService {
         this._inDanger$ = this._ongoingExposure$.pipe(
             map((exposure) => isInDanger(exposure))
         );
+        this.therapistPhone = `${global.ENV_THERAPIST_PHONE}`;
+        console.log("Therapist phone:", this.therapistPhone);
+    }
+
+    async callTherapist(): Promise<boolean> {
+        const phoneNumber = this.therapistPhone;
+        try {
+            await requestCallPermission(
+                "Necesitamos este permiso para poder llamar a tu terapeuta"
+            );
+
+            return this.dial(phoneNumber, false);
+        } catch (e) {
+            this.logger.error(`Call permission request failed. Reason: ${e}`);
+
+            return this.dial(phoneNumber, true);
+        }
+    }
+
+    private dial(phone: string, askPermission: boolean): boolean {
+        try {
+            dial(phone, askPermission);
+
+            return true;
+        } catch (e) {
+            this.logger.error(`Could not call therapist. Reason: ${e}`);
+
+            return false;
+        }
     }
 }
 
