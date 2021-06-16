463f4e5872df1cde7a8d2e0b786e357f244fb7af
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-06-16 16:30:12 +0200

    Freeze progress view while app is not visible

--- src/app/views/main/pages/progress/progress-container.component.ts
+++ src/app/views/main/pages/progress/progress-container.component.ts
@@ -1,11 +1,16 @@
 import { Component, NgZone, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 import { PatientDataService } from "~/app/views/patient-data.service";
 import { ExposureChange } from "~/app/tasks/exposure";
 import { RecordType } from "~/app/core/record-type";
 import { Change } from "@geotecinit/emai-framework/entities";
+import { getLogger, Logger } from "~/app/core/utils/logger";
+import { appEvents } from "~/app/core/app-events";
+import { Application } from "@nativescript/core";
+
+const APP_EVENTS_KEY = "ProgressContainer";
 
 @Component({
     selector: "SymProgressContainer",
@@ -17,16 +22,37 @@ export class ProgressContainerComponent implements OnInit, OnDestroy {
     underExposure: boolean;
 
     private exposureChangeSubscription?: Subscription;
+    private logger: Logger;
 
     constructor(
         private patientDataService: PatientDataService,
         private ngZone: NgZone
-    ) {}
+    ) {
+        this.logger = getLogger("ProgressContainer");
+    }
 
     ngOnInit() {
+        this.subscribeToExposureChanges();
+        appEvents.on(Application.resumeEvent, APP_EVENTS_KEY, () => {
+            this.subscribeToExposureChanges();
+        });
+
+        appEvents.on(Application.resumeEvent, APP_EVENTS_KEY, () => {
+            this.unsubscribeFromExposureChanges();
+        });
+    }
+
+    ngOnDestroy() {
+        this.unsubscribeFromExposureChanges();
+    }
+
+    private subscribeToExposureChanges() {
+        if (this.exposureChangeSubscription) return;
+
         this.exposureChangeSubscription = this.patientDataService
             .observeLastByRecordType<ExposureChange>(RecordType.ExposureChange)
             .pipe(
+                tap(() => console.log("Progress container change!")),
                 map(
                     (exposureChange) =>
                         !exposureChange || exposureChange.change === Change.END
@@ -38,9 +64,14 @@ export class ProgressContainerComponent implements OnInit, OnDestroy {
                     this.underExposure = !idle;
                 });
             });
+        this.logger.debug("Subscribed to exposure changes");
     }
 
-    ngOnDestroy() {
-        this.exposureChangeSubscription?.unsubscribe();
+    private unsubscribeFromExposureChanges() {
+        if (!this.exposureChangeSubscription) return;
+
+        this.exposureChangeSubscription.unsubscribe();
+        this.exposureChangeSubscription = undefined;
+        this.logger.debug("Unsubscribed from exposure changes");
     }
 }
