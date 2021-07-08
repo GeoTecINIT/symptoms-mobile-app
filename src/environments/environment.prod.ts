3bc654eb3e706a7ced15ac3819ac14937323a9ad
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-07-08 18:28:56 +0200

    Update environment files with API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: true
+    production: true,
+    serverHostname: "symptoms-api-gateway-oan3aj7yfa-uc.a.run.app",
 };
--- src/environments/environment.tst.ts
+++ src/environments/environment.tst.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: false
+    production: false,
+    serverHostname: "symptoms-api-gateway-oan3aj7yfa-uc.a.run.app",
 };
