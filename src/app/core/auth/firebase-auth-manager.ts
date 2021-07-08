3e97e49de69cec0dd141c1cf97ceb26c4f552502
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-07-08 18:26:08 +0200

    Update firebase auth manager to allow token acquisition

--- src/app/core/auth/firebase-auth-manager.ts
+++ src/app/core/auth/firebase-auth-manager.ts
@@ -1,14 +1,21 @@
 import { FirebaseManager, firebaseManager } from "../utils/firebase";
 import { Logger, getLogger } from "../utils/logger";
+import { firebase as fb } from "@nativescript/firebase";
+import User = fb.User;
 
-class AuthManager implements AuthManager {
+export interface AuthManager {
+    sessionData(): Promise<SessionData>;
+    authToken(): Promise<string>;
+}
+
+class FirebaseAuthManager implements AuthManager {
     private logger: Logger;
     private firebaseManager: FirebaseManager;
     private initPromise: Promise<any>;
     private initialized = false;
 
     constructor() {
-        this.logger = getLogger("AuthManager");
+        this.logger = getLogger("FirebaseAuthManager");
         this.firebaseManager = firebaseManager;
     }
 
@@ -23,7 +30,7 @@ class AuthManager implements AuthManager {
             });
         }
         const user = await this.initPromise;
-        const sessionData = this.sessionDataFromUser(user);
+        const sessionData = sessionDataFromUser(user);
         this.logger.info(`Logged in as: ${JSON.stringify(sessionData)}`);
         this.firebaseManager.crashlytics().then((crashlytics) => {
             if (crashlytics) {
@@ -34,35 +41,49 @@ class AuthManager implements AuthManager {
     }
 
     async sessionData(): Promise<SessionData> {
+        const user = await this.getCurrentUser();
+
+        return sessionDataFromUser(user);
+    }
+
+    async authToken(): Promise<string> {
+        const user = await this.getCurrentUser();
+
+        const token = await user.getIdToken();
+        this.logger.info(`Token: ${token}`);
+
+        return token;
+    }
+
+    private async getCurrentUser(): Promise<User> {
         await this.init();
         const firebase = await this.firebaseManager.getInstance();
         await firebase.reloadUser();
-        const user = await firebase.getCurrentUser();
 
-        return this.sessionDataFromUser(user);
-    }
-
-    private sessionDataFromUser(user: any): SessionData {
-        const {
-            uid,
-            additionalUserInfo: { isNewUser },
-            metadata: { creationTimestamp, lastSignInTimestamp },
-        } = user;
-
-        return {
-            id: uid,
-            isNew: isNewUser,
-            createdAt: creationTimestamp,
-            lastLogin: lastSignInTimestamp,
-        };
+        return firebase.getCurrentUser();
     }
 }
 
-export const authManager = new AuthManager();
+export const firebaseAuthManager = new FirebaseAuthManager();
 
 export interface SessionData {
     id: string;
     isNew: boolean;
     createdAt: Date;
     lastLogin: Date;
+}
+
+function sessionDataFromUser(user: any): SessionData {
+    const {
+        uid,
+        additionalUserInfo: { isNewUser },
+        metadata: { creationTimestamp, lastSignInTimestamp },
+    } = user;
+
+    return {
+        id: uid,
+        isNew: isNewUser,
+        createdAt: creationTimestamp,
+        lastLogin: lastSignInTimestamp,
+    };
 }
