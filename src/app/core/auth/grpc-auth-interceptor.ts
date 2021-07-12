62526115103309704348b4d21510e33c0f3a0bea
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-07-12 16:47:13 +0200

    Add method debugging to GRPCAuthInterceptor

--- src/app/core/auth/grpc-auth-interceptor.ts
+++ src/app/core/auth/grpc-auth-interceptor.ts
@@ -3,6 +3,7 @@ import {
     AuthManager,
     firebaseAuthManager,
 } from "~/app/core/auth/firebase-auth-manager.ts";
+import { getLogger } from "~/app/core/utils/logger";
 
 export class GRPCAuthInterceptor implements grpc.UnaryInterceptor<any, any> {
     constructor(private authManager: AuthManager = firebaseAuthManager) {}
@@ -13,6 +14,9 @@ export class GRPCAuthInterceptor implements grpc.UnaryInterceptor<any, any> {
             request: grpc.Request<any, any>
         ) => Promise<grpc.UnaryResponse<any, any>>
     ): Promise<grpc.UnaryResponse<any, any>> {
+        const logger = getLogger("GRPCAuthInterceptor");
+        logger.debug((request.getMethodDescriptor() as any).name);
+
         return this.authManager.authToken().then((token) => {
             const metadata = request.getMetadata();
             metadata.authorization = `Bearer ${token}`;
