02682db7167ffb9359532037163fcaceec312eef
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-07-12 16:45:36 +0200

    Add a patient profile controller for data sharing consent management

--- src/app/core/account/account.service.ts
+++ src/app/core/account/account.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
-import { deviceProfile, DeviceProfileStore } from "./device-profile";
+import { deviceProfile, DeviceProfileController } from "./device-profile";
+import { patientProfile, PatientProfileController } from "./patient-profile";
 
 @Injectable({
     providedIn: "root",
 })
 export class AccountService {
-    get deviceProfile(): DeviceProfileStore {
+    get deviceProfile(): DeviceProfileController {
         return deviceProfile;
+    }
+
+    get patientProfile(): PatientProfileController {
+        return patientProfile;
     }
 }
