9596dfc8becef75c73dbbfdadb8e125c2d11e999
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-09-30 19:10:52 +0200

    Update environment files with exposure parameters (30 min exp)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-    production: true
+    production: true,
+    exposureTimes: {
+        regular: 32,
+        extension: 15,
+    },
 };
--- src/environments/environment.tst.ts
+++ src/environments/environment.tst.ts
@@ -1,3 +1,7 @@
 export const environment = {
-    production: false
+    production: false,
+    exposureTimes: {
+        regular: 32,
+        extension: 15,
+    },
 };
