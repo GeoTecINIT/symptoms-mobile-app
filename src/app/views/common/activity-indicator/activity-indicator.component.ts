d5bec108ad572eb13c602bc1a9aba0f31ff7676e
Miguel <al341802@uji.es>
2021-10-07 10:02:52 +0200

    Add activity indicator component

---
+++ src/app/views/common/activity-indicator/activity-indicator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "SymActivityIndicator",
+  templateUrl: "./activity-indicator.component.html",
+  styleUrls: ["./activity-indicator.component.scss"]
+})
+export class ActivityIndicatorComponent {
+}
