e688ba071a251e524ceb47d3727da6b093e73996
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-12-27 21:04:36 +0100

    Add a mechanism to check if a pre-exposure is already running

--- src/app/tasks/exposure/escapes/exposure-status-checker.ts
+++ src/app/tasks/exposure/escapes/exposure-status-checker.ts
@@ -1,52 +1,17 @@
-import {
-    DispatchableEvent,
-    TaskOutcome,
-    TaskParams,
-    TraceableTask,
-} from "@geotecinit/emai-framework/tasks";
 import { exposures, ExposuresStore } from "~/app/core/persistence/exposures";
-import { AoIProximityChange } from "@geotecinit/emai-framework/entities/aois";
-import { checkIfProximityChangesInvolveOngoingExposure } from "~/app/tasks/exposure/escapes/common";
+import { AbstractExposureStatusChecker } from "~/app/tasks/exposure/escapes/abstract-exposure-status-checker";
 
 const TASK_NAME = "checkExposureAreaStatus";
 const ENTERED_WITH_NO_ONGOING_EXPOSURE = "enteredAreaWithNoOngoingExposure";
 const ENTERED_WITH_ONGOING_EXPOSURE = "enteredAreaWithOngoingExposure";
 
-export class ExposureStatusChecker extends TraceableTask {
-    constructor(private store: ExposuresStore = exposures) {
-        super(TASK_NAME, {
-            outputEventNames: [
-                `${TASK_NAME}Finished`,
-                ENTERED_WITH_NO_ONGOING_EXPOSURE,
-                ENTERED_WITH_ONGOING_EXPOSURE,
-            ],
-        });
-    }
-
-    protected async onTracedRun(
-        taskParams: TaskParams,
-        invocationEvent: DispatchableEvent
-    ): Promise<TaskOutcome> {
-        const changes = invocationEvent.data as Array<AoIProximityChange>;
-
-        const result = await checkIfProximityChangesInvolveOngoingExposure(
-            changes,
-            this.store
+export class ExposureStatusChecker extends AbstractExposureStatusChecker {
+    constructor(store: ExposuresStore = exposures) {
+        super(
+            TASK_NAME,
+            ENTERED_WITH_ONGOING_EXPOSURE,
+            ENTERED_WITH_NO_ONGOING_EXPOSURE,
+            store
         );
-
-        switch (result) {
-            case "no-change":
-                return { eventName: this.outputEventNames[0] };
-            case "not-present":
-                return {
-                    eventName: ENTERED_WITH_NO_ONGOING_EXPOSURE,
-                    result: changes,
-                };
-            case "present":
-                return {
-                    eventName: ENTERED_WITH_ONGOING_EXPOSURE,
-                    result: changes,
-                };
-        }
     }
 }
