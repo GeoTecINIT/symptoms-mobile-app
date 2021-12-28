e4cdc8b4fde2deabc4cb7268711c4d0ba639fd68
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-12-28 21:22:52 +0100

    Add binary option question type

--- src/app/core/modals/questions/options.ts
+++ src/app/core/modals/questions/options.ts
@@ -11,7 +11,7 @@ export interface QuestionsModalOptions {
     completionScreen?: CompletionScreenOptions;
 }
 
-export type QuestionType = RangeQuestion | FreeTextQuestion; // Append other question types here
+export type QuestionType = RangeQuestion | FreeTextQuestion | BinaryQuestion; // Append other question types here
 
 interface Question {
     title: string;
@@ -27,4 +27,11 @@ interface FreeTextQuestion extends Question {
     type: "free-text";
     hint?: string;
     helpText?: string;
+}
+
+interface BinaryQuestion extends Question {
+    type: "binary";
+    left: string;
+    right: string;
+    default?: string;
 }
