60adb22f1273b0c9f713557aa827e974b2678dbd
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-12-28 16:36:27 +0100

    Add pre-exposure questions modal

--- src/app/core/modals/questions/modals.ts
+++ src/app/core/modals/questions/modals.ts
@@ -1,6 +1,6 @@
 import { QuestionsModalOptions } from "./options";
 
-export const askAnxietyQuestions: QuestionsModalOptions = {
+export const askPreExposureQuestions: QuestionsModalOptions = {
     title: "¿Cómo te encuentras?",
     body: {
         iconCode: "\ue94c",
@@ -23,7 +23,38 @@ export const askAnxietyQuestions: QuestionsModalOptions = {
         },
         {
             title:
-                "De 0 a 10, ¿cómo puntuarías el grado de creencia en los pensamientos que tienes acerca de esta situación?",
+                "¿Qué piensas que ocurrirá cuando llegues al lugar de exposición?",
+            type: "free-text",
+            hint: "Pienso que...",
+        },
+    ],
+    completionScreen: {
+        body: {
+            iconCode: "\ue815",
+            header: "¡Adelante!",
+            message: "Dirígete ahora al área de exposición",
+        },
+        confirmButton: "Volver a la app",
+    },
+};
+
+export const askExposureQuestions: QuestionsModalOptions = {
+    title: "¿Cómo te encuentras?",
+    body: {
+        iconCode: "\ue94c",
+        text: "Responde con sinceridad a las preguntas que te planteamos",
+    },
+    questions: [
+        {
+            title:
+                "De 0 a 10, ¿cómo puntuarías tu nivel de ansiedad en este momento?",
+            type: "range",
+            from: 0,
+            to: 10,
+        },
+        {
+            title:
+                "De 0 a 10, ¿cómo puntuarías las ganas que tienes de salir de aquí?",
             type: "range",
             from: 0,
             to: 10,
