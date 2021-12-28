8107fddb85dd8991a26681e4a32eff5c6ff0a8d9
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-12-28 16:34:16 +0100

    Extract completion screen modal component

--- src/app/views/main/modals/common/common-modals.module.ts
+++ src/app/views/main/modals/common/common-modals.module.ts
@@ -3,11 +3,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "@nativescript/angular";
 import { ModalBodyComponent } from "./modal-body/modal-body.component";
 import { CommonComponentsModule } from "~/app/views/common/common-components.module";
+import { CompletionScreenComponent } from "./completion-screen/completion-screen.component";
 
 @NgModule({
     imports: [NativeScriptCommonModule, CommonComponentsModule],
-    declarations: [ModalBodyComponent],
+    declarations: [ModalBodyComponent, CompletionScreenComponent],
     schemas: [NO_ERRORS_SCHEMA],
-    exports: [ModalBodyComponent],
+    exports: [ModalBodyComponent, CompletionScreenComponent],
 })
 export class CommonModalsModule {}
