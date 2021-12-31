7af79644fb4daec4a40aaf545978ca8306da9080
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-12-31 11:22:41 +0100

    Add new content container with tabs for guidelines and psychoeducations

---
+++ src/app/views/main/pages/content/content-container.component.ts
@@ -0,0 +1,68 @@
+import { Component, HostListener, NgZone } from "@angular/core";
+import {
+    TreatmentContent,
+    TreatmentContentService,
+} from "~/app/views/treatment-content.service";
+import { Subject } from "rxjs";
+import { ContentViewModalService } from "~/app/views/main/modals/content-view";
+import { takeUntil } from "rxjs/operators";
+import { getLogger, Logger } from "~/app/core/utils/logger";
+
+@Component({
+    selector: "SymContentContainer",
+    templateUrl: "./content-container.component.html",
+    styleUrls: ["./content-container.component.scss"],
+})
+export class ContentContainerComponent {
+    guidelines: Array<TreatmentContent>;
+    psychoeducations: Array<TreatmentContent>;
+
+    private unloaded$ = new Subject();
+    private logger: Logger;
+
+    constructor(
+        private treatmentContentService: TreatmentContentService,
+        private contentViewModalService: ContentViewModalService,
+        private ngZone: NgZone
+    ) {
+        this.logger = getLogger("ContentContainerComponent");
+    }
+
+    @HostListener("loaded")
+    onLoaded() {
+        this.subscribeToContentChanges();
+    }
+
+    @HostListener("unloaded")
+    onUnloaded() {
+        this.unloaded$.next();
+    }
+
+    onContentTap(content: TreatmentContent) {
+        const { id } = content;
+        this.contentViewModalService
+            .showContent(id)
+            .catch((err) =>
+                this.logger.error(
+                    `Could not show content (${id}). Reason: ${err}`
+                )
+            );
+    }
+
+    private subscribeToContentChanges() {
+        this.treatmentContentService.guideliness$
+            .pipe(takeUntil(this.unloaded$))
+            .subscribe((guidelines) => {
+                this.ngZone.run(() => {
+                    this.guidelines = guidelines;
+                });
+            });
+        this.treatmentContentService.psychoeducations$
+            .pipe(takeUntil(this.unloaded$))
+            .subscribe((psychoeducations) => {
+                this.ngZone.run(() => {
+                    this.psychoeducations = psychoeducations;
+                });
+            });
+    }
+}
--- src/app/views/main/pages/content/content.module.ts
+++ src/app/views/main/pages/content/content.module.ts
@@ -2,15 +2,25 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
 import { ContentRoutingModule } from "./content-routing.module";
 import { NativeScriptCommonModule } from "@nativescript/angular";
+import { NativeScriptMaterialTabsModule } from "@nativescript-community/ui-material-tabs/angular";
 
 import { CommonMainModule } from "../../common/common-main.module";
-
-import { ContentListComponent } from "./content-list.component";
-import { ContentListItemComponent } from "./content-list-item/content-list-item.component";
+import { ContentContainerComponent } from "./content-container.component";
+import { ContentListComponent } from "./content-list/content-list.component";
+import { ContentListItemComponent } from "./content-list/content-list-item/content-list-item.component";
 
 @NgModule({
-    imports: [ContentRoutingModule, NativeScriptCommonModule, CommonMainModule],
-    declarations: [ContentListComponent, ContentListItemComponent],
+    imports: [
+        ContentRoutingModule,
+        NativeScriptCommonModule,
+        NativeScriptMaterialTabsModule,
+        CommonMainModule,
+    ],
+    declarations: [
+        ContentContainerComponent,
+        ContentListComponent,
+        ContentListItemComponent,
+    ],
     schemas: [NO_ERRORS_SCHEMA],
 })
 export class ContentModule {}
