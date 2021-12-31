b640d049bbe1b980db126391259f04be33b4eae0
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2021-12-31 12:02:56 +0100

    Abstract main view tab selection logic to its service

--- src/app/views/main/main-view.service.ts
+++ src/app/views/main/main-view.service.ts
@@ -1,16 +1,39 @@
 import { Injectable, ViewContainerRef, Type } from "@angular/core";
 import { ModalDialogService, ModalDialogOptions } from "@nativescript/angular";
 
+export enum NavigationTab {
+    Progress,
+    Content,
+    Notifications,
+}
+
+export type TabSelectionCallback = (tab: NavigationTab) => void;
+
 @Injectable({
     providedIn: "root",
 })
 export class MainViewService {
+    get selectedTab(): NavigationTab {
+        return this._selectedTab;
+    }
+
     private viewContainerRef: ViewContainerRef;
+    private _selectedTab = NavigationTab.Progress;
+    private tabSelectionCallback: TabSelectionCallback;
 
     constructor(private modalService: ModalDialogService) {}
 
     setViewContainerRef(vcRef: ViewContainerRef) {
         this.viewContainerRef = vcRef;
+    }
+
+    setSelectedTab(tab: NavigationTab) {
+        this._selectedTab = tab;
+        if (this.tabSelectionCallback) this.tabSelectionCallback(tab);
+    }
+
+    onTabSelected(cb: TabSelectionCallback) {
+        this.tabSelectionCallback = cb;
     }
 
     showFullScreenAnimatedModal(
