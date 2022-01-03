e858c07cca7d064593e9f90aa874e9f1c46e995b
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2022-01-03 22:26:33 +0100

    Add mapbox plugin

--- reference.d.ts
+++ reference.d.ts
@@ -1,8 +1,6 @@
 /// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
 /// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
 
-/// <reference path="native_declarations/android-play-services-location.d.ts" />
-
 declare namespace NodeJS {
     interface Global {
         ENV_NAME: string;
