843a741cf92204b8b1147ab0fb5eb294ead993d9
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2022-02-02 22:15:23 +0100

    Add new advanced settings container component

---
+++ src/app/views/main/modals/settings/advanced-settings-container/advanced-settings-container.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "SymAdvancedSettingsContainer",
+    templateUrl: "./advanced-settings-container.component.html",
+    styleUrls: ["./advanced-settings-container.component.scss"],
+})
+export class AdvancedSettingsContainerComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
