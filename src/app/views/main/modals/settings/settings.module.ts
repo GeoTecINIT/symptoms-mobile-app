b9767df48d36b63b560e13e89100b982984490db
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2022-02-02 22:15:56 +0100

    Integrate new advanced settings container component

--- src/app/views/main/modals/settings/settings.module.ts
+++ src/app/views/main/modals/settings/settings.module.ts
@@ -7,7 +7,9 @@ import { CommonComponentsModule } from "~/app/views/common/common-components.mod
 import { AppSettingsOptionsModule } from "~/app/views/common/app-settings-options/app-settings-options.module";
 
 import { SettingsContainerComponent } from "./settings-container/settings-container.component";
-import { SettingsSectionComponent } from "./settings-container/settings-section/settings-section.component";
+import { SettingsSectionComponent } from "./settings-section/settings-section.component";
+import { AdvancedSettingsContainerComponent } from "./advanced-settings-container/advanced-settings-container.component";
+import { PanicButtonOptionComponent } from './advanced-settings-container/panic-button-option/panic-button-option.component';
 
 @NgModule({
     imports: [
@@ -16,7 +18,12 @@ import { SettingsSectionComponent } from "./settings-container/settings-section/
         CommonComponentsModule,
         AppSettingsOptionsModule,
     ],
-    declarations: [SettingsContainerComponent, SettingsSectionComponent],
+    declarations: [
+        SettingsContainerComponent,
+        SettingsSectionComponent,
+        AdvancedSettingsContainerComponent,
+        PanicButtonOptionComponent,
+    ],
     schemas: [NO_ERRORS_SCHEMA],
 })
 export class SettingsModule {}
