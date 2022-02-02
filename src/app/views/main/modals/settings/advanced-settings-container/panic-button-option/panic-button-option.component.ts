97fc38d6809bce7e5bf0310cd7d410f51cda1d71
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2022-02-02 22:14:59 +0100

    Add new panic button activation setting component

---
+++ src/app/views/main/modals/settings/advanced-settings-container/panic-button-option/panic-button-option.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from "@angular/core";
+import { AdvancedSetting, AdvancedSettingsService } from "~/app/core/account";
+import { getLogger, Logger } from "~/app/core/utils/logger";
+
+@Component({
+    selector: "SymPanicButtonOption",
+    templateUrl: "./panic-button-option.component.html",
+    styleUrls: ["./panic-button-option.component.scss"],
+})
+export class PanicButtonOptionComponent {
+    @Input() marginBottom: number;
+    @Input() bold = false;
+    @Input() italic = false;
+
+    panicButtonActive: boolean;
+
+    private logger: Logger;
+
+    constructor(private advancedSettingsService: AdvancedSettingsService) {
+        this.logger = getLogger("DataSharingOptionComponent");
+        this.panicButtonActive = this.advancedSettingsService.getBoolean(
+            AdvancedSetting.PanicButton
+        );
+    }
+
+    onDataSharingCheckChange(checked: boolean) {
+        this.advancedSettingsService
+            .setBoolean(AdvancedSetting.PanicButton, checked)
+            .catch((e) =>
+                this.logger.error(
+                    `Could not update panic button active status. Reason: ${e}`
+                )
+            );
+    }
+}
