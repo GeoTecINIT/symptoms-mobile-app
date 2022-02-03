6254f08bc86cb7dbc78efe6e8a891b85d46b8273
Alberto González <10727467+agonper@users.noreply.github.com>
2022-02-03 13:18:45 +0100

    Add a component to customize numeric settings

---
+++ src/app/views/common/app-settings-options/generic-settings-number/generic-settings-number.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+    inputType,
+    PromptOptions,
+    PromptResult,
+    prompt,
+} from "@nativescript/core";
+
+@Component({
+    selector: "SymGenericSettingsNumber",
+    templateUrl: "./generic-settings-number.component.html",
+    styleUrls: ["./generic-settings-number.component.scss"],
+})
+export class GenericSettingsNumberComponent {
+    @Input() text = "";
+    @Input()
+    set value(num: number) {
+        this.currentValue = num;
+    }
+    @Input() unit: string;
+    @Input() min: number;
+
+    @Input() bold = false;
+    @Input() italic = false;
+    @Input() marginBottom: number;
+
+    @Output() valueChange = new EventEmitter<number>();
+
+    currentValue: number;
+
+    onWantsToUpdateValue() {
+        const minText = this.min !== undefined ? ` (min: ${this.min})` : "";
+        let options: PromptOptions = {
+            title: `Introduce un nuevo valor${minText}`,
+            defaultText: `${this.currentValue}`,
+            okButtonText: "Confirmar",
+            cancelButtonText: "Cancelar",
+            cancelable: true,
+            inputType: inputType.number,
+        };
+
+        prompt(options).then((result: PromptResult) => {
+            if (!result.result) return;
+
+            const newValue = parseFloat(result.text);
+            if (newValue < this.min) return;
+
+            this.currentValue = newValue;
+            this.valueChange.emit(newValue);
+        });
+    }
+}
