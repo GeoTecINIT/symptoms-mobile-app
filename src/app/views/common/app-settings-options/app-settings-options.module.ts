4240fad3128da3bcc93511b9da50755a8914fb2e
Alberto González <10727467+agonper@users.noreply.github.com>
2022-02-03 13:18:45 +0100

    Add a component to customize numeric settings

--- src/app/views/common/app-settings-options/app-settings-options.module.ts
+++ src/app/views/common/app-settings-options/app-settings-options.module.ts
@@ -7,6 +7,7 @@ import {
 import { GenericSettingsOptionComponent } from "./generic-settings-option/generic-settings-option.component";
 import { DataSharingOptionComponent } from "./data-sharing-option/data-sharing-option.component";
 import { ReportUsageOptionComponent } from "./report-usage-option/report-usage-option.component";
+import { GenericSettingsNumberComponent } from "./generic-settings-number/generic-settings-number.component";
 
 @NgModule({
     imports: [NativeScriptCommonModule, NativeScriptRouterModule],
@@ -14,12 +15,14 @@ import { ReportUsageOptionComponent } from "./report-usage-option/report-usage-o
         GenericSettingsOptionComponent,
         DataSharingOptionComponent,
         ReportUsageOptionComponent,
+        GenericSettingsNumberComponent,
     ],
     schemas: [NO_ERRORS_SCHEMA],
     exports: [
         GenericSettingsOptionComponent,
         DataSharingOptionComponent,
         ReportUsageOptionComponent,
+        GenericSettingsNumberComponent,
     ],
 })
 export class AppSettingsOptionsModule {}
