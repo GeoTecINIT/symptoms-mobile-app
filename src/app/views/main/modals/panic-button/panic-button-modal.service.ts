ffa5938e4534a76c420c4a87959071e58b71d408
Alberto Gonzalez <10727467+agonper@users.noreply.github.com>
2022-02-05 14:41:33 +0100

    Add panic button modal service

---
+++ src/app/views/main/modals/panic-button/panic-button-modal.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { getLogger, Logger } from "~/app/core/utils/logger";
+import { MainViewService } from "~/app/views/main/main-view.service";
+import { PanicButtonModalComponent } from "~/app/views/main/modals/panic-button/panic-button-modal.component.ts";
+
+@Injectable({
+    providedIn: "root",
+})
+export class PanicButtonModalService {
+    private logger: Logger;
+
+    constructor(private mainViewService: MainViewService) {
+        this.logger = getLogger("PanicButtonService");
+    }
+
+    show() {
+        this.mainViewService
+            .showFullScreenAnimatedModal(PanicButtonModalComponent)
+            .catch((e) =>
+                this.logger.error(
+                    `Error while opening panic button modal: ${e}`
+                )
+            );
+    }
+}
