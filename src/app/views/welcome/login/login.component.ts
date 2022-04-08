b97185f720d8e148869d52d2b9a33112a905e3e9
Alberto González <10727467+agonper@users.noreply.github.com>
2022-04-08 16:15:07 +0100

    fix: imports with extensions

--- src/app/views/welcome/login/login.component.ts
+++ src/app/views/welcome/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { InAppBrowserService } from "~/app/views/common/in-app-browser.service";
-import { AuthService } from "~/app/views/auth.service.ts";
+import { AuthService } from "~/app/views/auth.service";
 import { NavigationService } from "~/app/views/navigation.service";
 import { ActivatedRoute } from "@angular/router";
 
