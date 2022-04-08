bad4682a3025d58979e551928cf0cba8b48f5cca
Alberto González <10727467+agonper@users.noreply.github.com>
2022-04-08 17:23:04 +0100

    fix: webpack config regression

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const webpack = require("@nativescript/webpack");
 
 module.exports = (env) => {
-	webpack.init(env);
+  webpack.init(env);
 
-	// Learn how to customize:
-	// https://docs.nativescript.org/webpack
+  // Learn how to customize:
+  // https://docs.nativescript.org/webpack
 
-	return webpack.resolveConfig();
+  webpack.chainWebpack((config) => {
+    config.plugin("DefinePlugin").tap((args) => {
+      const mode = env.production ? "production" : "development";
+      Object.assign(args[0], {
+        "global.ENV_NAME": JSON.stringify(mode || "development"),
+        "global.ENV_THERAPIST_PHONE": JSON.stringify(env.therapistPhone || ""),
+      });
+
+      return args;
+    });
+  });
+
+  return webpack.resolveConfig();
 };
-
-
