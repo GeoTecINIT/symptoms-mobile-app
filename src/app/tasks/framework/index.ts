db7f4be74bdc781ec87b2ff6262c3e9a7099254c
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-04 12:49:33 +0100

    chore: migrate to awarns framework alpha.3

--- src/app/tasks/framework/index.ts
+++ src/app/tasks/framework/index.ts
@@ -1,4 +1,8 @@
 import { Task } from "@awarns/core/tasks";
 import { geofencingTask } from "@awarns/geofencing";
+import { singleGeolocationTask } from "@awarns/geolocation";
 
-export const awarnsTasks: Array<Task> = [geofencingTask()];
+export const awarnsTasks: Array<Task> = [
+    singleGeolocationTask({ bestOf: 3, timeout: 10000 }),
+    geofencingTask(),
+];
