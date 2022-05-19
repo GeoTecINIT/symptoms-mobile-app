3b19784ca9ca1dfaf26b6198f5abf967999aec8c
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-19 18:44:13 +0100

    refactor: use native methods in visualization data calculation tasks

--- src/app/tasks/visualizations/calc-exposure-place-aggregate.ts
+++ src/app/tasks/visualizations/calc-exposure-place-aggregate.ts
@@ -4,7 +4,7 @@ import {
     TaskOutcome,
     TaskParams,
 } from "@awarns/core/tasks";
-import { patientData, PatientData } from "~/app/core/framework/patient-data";
+import { recordsStore, RecordsStore } from "@awarns/persistence";
 import { ExposureChange } from "~/app/tasks/exposure";
 import { EmotionValue } from "~/app/core/persistence/exposures";
 import { calculateEmotionValuesAvg } from "~/app/tasks/visualizations/common";
@@ -13,7 +13,7 @@ import { AppRecordType } from "~/app/core/app-record-type";
 import { firstValueFrom } from "rxjs";
 
 export class CalculateExposurePlaceAggregate extends Task {
-    constructor(private store: PatientData = patientData) {
+    constructor(private store: RecordsStore = recordsStore) {
         super("calculateExposurePlaceAggregate", {
             outputEventNames: ["exposurePlaceAggregateCalculated"],
         });
@@ -33,12 +33,11 @@ export class CalculateExposurePlaceAggregate extends Task {
             value: calculateEmotionValuesAvg(emotionValues),
         };
 
-        const prevAggregate = await firstValueFrom(
-            this.store.observeLastByRecordType<ExposurePlaceAggregate>(
-                AppRecordType.ExposurePlaceAggregate,
-                [{ property: "placeId", comparison: "=", value: id }]
-            )
-        );
+        const prevAggregate = (await firstValueFrom(
+            this.store.listLast(AppRecordType.ExposurePlaceAggregate, [
+                { property: "placeId", comparison: "=", value: id },
+            ])
+        )) as ExposurePlaceAggregate;
 
         const updatedEmotionValues = prevAggregate
             ? [...prevAggregate.emotionValues, newEntry]
