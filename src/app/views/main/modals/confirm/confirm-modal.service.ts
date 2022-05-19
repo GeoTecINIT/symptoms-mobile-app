10cbe07b66719df687380210f5dadd17f91cb328
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-19 14:47:50 +0100

    feat: user modal confirmations are propagated through the ntd

--- src/app/views/main/modals/confirm/confirm-modal.service.ts
+++ src/app/views/main/modals/confirm/confirm-modal.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from "@angular/core";
 import { MainViewService } from "../../main-view.service";
 import { ConfirmModalComponent } from "~/app/views/main/modals/confirm/confirm-modal.component";
 import { ConfirmModalOptions } from "~/app/core/modals/confirm";
+import { emitPatientConfirmationAcquiredEvent } from "~/app/core/framework/events";
+import { UserConfirmation } from "@awarns/notifications";
 
 @Injectable({
     providedIn: "root",
@@ -10,10 +12,24 @@ import { ConfirmModalOptions } from "~/app/core/modals/confirm";
 export class ConfirmModalService {
     constructor(private mainViewService: MainViewService) {}
 
-    show(options: ConfirmModalOptions): Promise<boolean> {
-        return this.mainViewService.showFullScreenAnimatedModal(
-            ConfirmModalComponent,
-            options
-        );
+    show(
+        confirmationId: string,
+        options: ConfirmModalOptions,
+        notificationId?: number
+    ): Promise<boolean> {
+        return this.mainViewService
+            .showFullScreenAnimatedModal(ConfirmModalComponent, options)
+            .then((result) => {
+                emitPatientConfirmationAcquiredEvent(
+                    new UserConfirmation(
+                        confirmationId,
+                        options.question,
+                        options.negative ? !result : result,
+                        notificationId
+                    )
+                );
+
+                return result;
+            });
     }
 }
