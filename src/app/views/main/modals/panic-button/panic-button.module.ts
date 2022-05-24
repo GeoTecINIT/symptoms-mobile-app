40e9c54328893c93d1aba47307fe874f3b3f1825
Alberto González <10727467+agonper@users.noreply.github.com>
2022-05-24 18:47:48 +0100

    feat: panic button view now is a page rather than a native modal

--- src/app/views/main/modals/panic-button/panic-button.module.ts
+++ src/app/views/main/modals/panic-button/panic-button.module.ts
@@ -3,20 +3,20 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { PanicButtonRoutingModule } from "./panic-button-routing.module";
 import { NativeScriptCommonModule } from "@nativescript/angular";
 import { CommonComponentsModule } from "~/app/views/common/common-components.module";
-import { ContentDeliveryComponent } from "./pages/content-delivery/content-delivery.component";
-import { MakeContactComponent } from "./pages/make-contact/make-contact.component";
+import { PanicButtonModalComponent } from "./panic-button-modal.component";
+import { MakeContactComponent } from "./make-contact/make-contact.component";
 import { PanicButtonContentComponent } from "./common/panic-button-content/panic-button-content.component";
 
 @NgModule({
-    declarations: [
-        ContentDeliveryComponent,
-        MakeContactComponent,
-        PanicButtonContentComponent,
-    ],
     imports: [
         PanicButtonRoutingModule,
         NativeScriptCommonModule,
         CommonComponentsModule,
+    ],
+    declarations: [
+        PanicButtonModalComponent,
+        MakeContactComponent,
+        PanicButtonContentComponent,
     ],
     schemas: [NO_ERRORS_SCHEMA],
 })
