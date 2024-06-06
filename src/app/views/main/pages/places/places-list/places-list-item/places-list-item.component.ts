3769caf31b12999d8830e4c364f6b9bd5168c987
nazarpysko <nazarpysko27@gmail.com>
2024-06-06 11:24:05 +0200

    feat(list-places): added exposures counter

--- src/app/views/main/pages/places/places-list/places-list-item/places-list-item.component.ts
+++ src/app/views/main/pages/places/places-list/places-list-item/places-list-item.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, NgZone, HostListener } from "@angular/core";
 import { AreaOfInterest } from "@awarns/geofencing";
+import { Record, Change } from "@awarns/core/entities";
+
+import { Subject, takeUntil } from "rxjs";
+import { AppRecordType } from "~/app/core/app-record-type";
+import { FetchCondition, recordsStore } from "@awarns/persistence";
 
 @Component({
     selector: "SymPlacesListItem",
@@ -8,4 +13,41 @@ import { AreaOfInterest } from "@awarns/geofencing";
 })
 export class PlacesListItemComponent {
     @Input() place: AreaOfInterest;
+
+    records: Array<Record> = [];
+
+    private unloaded$ = new Subject<void>();
+
+    constructor(private ngZone: NgZone) {}
+
+    @HostListener("loaded")
+    onLoaded() {
+        this.subscribeToRecordChanges();
+    }
+
+    @HostListener("unloaded")
+    onUnloaded() {
+        this.unloaded$.next();
+    }
+
+    private subscribeToRecordChanges() {
+        console.log("Place id 👉👉👉", this.place.id)
+        const conditions: Array<FetchCondition> = [
+            { property: "change", comparison: "=", value: Change.END },
+            { property: "successful", comparison: "=", value: true },
+            { property: "place.id", comparison: "=", value: this.place.id }
+        ];
+        recordsStore
+            .listBy(AppRecordType.ExposureChange, "desc", conditions)
+            .pipe(takeUntil(this.unloaded$))
+            .subscribe((records) => {
+                this.ngZone.run(() => {
+                    this.records = records;
+                });
+            });
+    }
+
+    get numberOfExposures(): number {
+        return this.records.length;
+    }
 }
