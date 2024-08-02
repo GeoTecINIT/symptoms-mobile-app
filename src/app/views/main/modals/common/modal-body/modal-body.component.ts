63f999e4805b07003351e17bc738ff93bdb9bad0
Lucas Niño Ruiz <lnino@uji.es>
2024-08-02 14:05:41 +0200

    feat(contextual-modifiers): updated contextual modifiers check view before exposure

--- src/app/views/main/modals/common/modal-body/modal-body.component.ts
+++ src/app/views/main/modals/common/modal-body/modal-body.component.ts
@@ -7,6 +7,7 @@ import {
 import { ContextualConditions, WeatherSummary } from "~/app/core/weather";
 import { ModalDialogOptions, ModalDialogService } from "@nativescript/angular";
 import { ContextualConditionsComplianceDialogComponent } from "./contextual-conditions-compliance-dialog/contextual-conditions-compliance-dialog.component";
+import { Weather, LightCondition } from "~/app/core/weather";
 
 @Component({
     selector: "SymModalBody",
@@ -26,6 +27,7 @@ export class ModalBodyComponent {
 
     showingImage: boolean = false;
     vcRef: ViewContainerRef;
+    currentHour: string;
 
     constructor(
         private cd: ChangeDetectorRef,
@@ -41,10 +43,28 @@ export class ModalBodyComponent {
         return Object.keys(this.contextualConditions).length !== 0;
     }
 
+    get timeRangeFromContextualConditions(): string {
+        const timeRange = this.contextualConditions.timeRange;
+        if (timeRange === undefined) return undefined;
+
+        const startTime = this.contextualConditions.timeRange.startTime;
+        const endTime = this.contextualConditions.timeRange.endTime;
+
+        const startHours = startTime.hours.toString().padStart(2, "0");
+        const startMinutes = startTime.minutes.toString().padStart(2, "0");
+        const endHours = endTime.hours.toString().padStart(2, "0");
+        const endMinutes = endTime.minutes.toString().padStart(2, "0");
+
+        return `${startHours}:${startMinutes} a ${endHours}:${endMinutes}`;
+    }
+
     get isCurrentHourWithinRange(): boolean {
         const now = new Date();
+
         const currentHours = now.getHours();
         const currentMinutes = now.getMinutes();
+
+        this.currentHour = currentHours + ":" + currentMinutes;
 
         const startHours = this.contextualConditions.timeRange.startTime.hours;
         const startMinutes =
@@ -67,6 +87,148 @@ export class ModalBodyComponent {
                 currentTotalMinutes >= startTotalMinutes ||
                 currentTotalMinutes <= endTotalMinutes
             );
+        }
+    }
+
+    get lightConditionFromContextualConditions(): string {
+        const lightCondition = this.contextualConditions.lightCondition;
+        if (lightCondition === undefined) return undefined;
+
+        switch (lightCondition.value) {
+            case LightCondition.SUNRISE:
+                return "Amanecer";
+            case LightCondition.SUNSET:
+                return "Atardecer";
+            case LightCondition.DAY:
+                return "Día";
+            case LightCondition.NIGHT:
+                return "Noche";
+        }
+    }
+
+    get lightConditionFromWeatherSummary(): string {
+        const lightCondition = this.weatherSummary.lightCondition;
+        if (lightCondition === undefined) return undefined;
+
+        switch (lightCondition) {
+            case LightCondition.SUNRISE:
+                return "Amanecer";
+            case LightCondition.SUNSET:
+                return "Atardecer";
+            case LightCondition.DAY:
+                return "Día";
+            case LightCondition.NIGHT:
+                return "Noche";
+        }
+    }
+
+    get weatherAndWindFromContextualConditions(): string {
+        const weather = this.getWeatherFromContextualConditions();
+        const wind = this.getWindFromContextualConditions();
+        let text = "";
+
+        if (!weather && !wind) return undefined;
+        else if (!weather) text += wind;
+        else if (!wind) text += weather;
+        else
+            text += `${this.getWeatherFromContextualConditions()} ${this.getWindFromContextualConditions()}`;
+
+        return text;
+    }
+
+    get weatherAndWindFromWeatherSummary(): string {
+        const weather = this.getWeatherFromWeatherSummary();
+        const wind = this.getWindFromWeatherSummary();
+        let text = "";
+
+        if (!weather && !wind) return undefined;
+        else if (!weather) text += wind;
+        else if (!wind) text += weather;
+        else
+            text += `${this.getWeatherFromWeatherSummary()} ${this.getWindFromWeatherSummary()}`;
+
+        return text;
+    }
+
+    private getWeatherFromContextualConditions(): string {
+        if (this.contextualConditions.weather === undefined) return "";
+        switch (this.contextualConditions.weather) {
+            case Weather.THUNDERSTORM:
+                return "Tormenta eléctrica";
+            case Weather.DRIZZLE:
+                return "Llovizna";
+            case Weather.RAIN:
+                return "Lluvia";
+            case Weather.SNOW:
+                return "Nieve";
+            case Weather.MIST:
+                return "Niebla";
+            case Weather.SMOKE:
+                return "Humo";
+            case Weather.DUST:
+                return "Polvo";
+            case Weather.SAND:
+                return "Arena";
+            case Weather.ASH:
+                return "Ceniza";
+            case Weather.SQUALL:
+                return "Chubasco";
+            case Weather.TORNADO:
+                return "Tornado";
+            case Weather.CLEAR:
+                return "Despejado";
+            case Weather.CLOUDS:
+                return "Nublado";
+        }
+    }
+
+    private getWeatherFromWeatherSummary(): string {
+        if (this.contextualConditions.weather === undefined) return "";
+        switch (this.contextualConditions.weather) {
+            case Weather.THUNDERSTORM:
+                return "Tormenta eléctrica";
+            case Weather.DRIZZLE:
+                return "Llovizna";
+            case Weather.RAIN:
+                return "Lluvia";
+            case Weather.SNOW:
+                return "Nieve";
+            case Weather.MIST:
+                return "Niebla";
+            case Weather.SMOKE:
+                return "Humo";
+            case Weather.DUST:
+                return "Polvo";
+            case Weather.SAND:
+                return "Arena";
+            case Weather.ASH:
+                return "Ceniza";
+            case Weather.SQUALL:
+                return "Chubasco";
+            case Weather.TORNADO:
+                return "Tornado";
+            case Weather.CLEAR:
+                return "Despejado";
+            case Weather.CLOUDS:
+                return "Nublado";
+        }
+    }
+
+    private getWindFromContextualConditions(): string {
+        if (this.contextualConditions.isWindy === undefined) return "";
+        if (this.contextualConditions.isWindy) {
+            return "con viento";
+        } else {
+            return "sin viento";
+        }
+    }
+
+    private getWindFromWeatherSummary(): string {
+        if (this.weatherSummary.isWindy === undefined) return "";
+        if (this.weatherSummary.isWindy) {
+            return "con viento";
+        } else {
+            return "sin viento";
         }
     }
 
