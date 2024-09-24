d9f8e94c51bca925d804012aafa96a5e2228689c
nazarpysko <nazarpysko27@gmail.com>
2024-09-24 12:32:45 +0200

    missing file pending to add

--- src/app/tasks/feedback/index.ts
+++ src/app/tasks/feedback/index.ts
@@ -4,6 +4,7 @@ import { FeedbackDeliveryLimiter } from "~/app/tasks/feedback/feedback-delivery-
 import { CustomNotification } from "~/app/tasks/feedback/custom-notifications";
 import { ClearNotifications } from "~/app/tasks/feedback/clear-notifications";
 import { SendNotificationNthExposure } from "~/app/tasks/feedback/send-notification-nth-exposure"
+import { SendNotificationWithGeolocation } from "~/app/tasks/feedback/send-notification-geolocation"
 import { makeTraceable } from "@awarns/tracing";
 
 export const feedbackTasks: Array<Task> = [
@@ -12,6 +13,7 @@ export const feedbackTasks: Array<Task> = [
         new FeedbackDeliveryLimiter(), 
         new CustomNotification(),
         new ClearNotifications(),
-        new SendNotificationNthExposure()
+        new SendNotificationNthExposure(),
+        new SendNotificationWithGeolocation()
     ]),
 ];
