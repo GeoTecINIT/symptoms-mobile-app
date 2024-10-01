8a458f3434e69a3fd1730758256ff21fa1824486
nazarpysko <nazarpysko27@gmail.com>
2024-10-01 10:36:23 +0200

    feat: modified clear-notifications task in order to remove notifications after custom threshold

--- src/app/tasks/feedback/clear-notifications.ts
+++ src/app/tasks/feedback/clear-notifications.ts
@@ -17,10 +17,12 @@ export class ClearNotifications extends Task {
     ): Promise<void> {
         const notifications = await firstValueFrom(notificationsStore.list())
 
+        // If thresholdToRemoveInSeconds is not passed in the task params, 60 seconds is the default value
+        const { thresholdToRemoveInSeconds } = taskParams ?? { thresholdToRemoveInSeconds: 60 };
+
         const now = new Date().getTime();
-        const oneMinInMilliseconds = 1 * 60 * 1000;
         
         // Mark as seen notifications that are older than 1 min from current timestamp (now)
-        notifications.filter(n => (now - n.timestamp.getTime()) > oneMinInMilliseconds ).forEach(n => notificationsManager.markAsSeen(n.id))
+        notifications.filter(n => (now - n.timestamp.getTime()) / 1000 > thresholdToRemoveInSeconds ).forEach(n => notificationsManager.markAsSeen(n.id))
     }
 }
