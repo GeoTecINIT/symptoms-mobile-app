29b82c38f6fd09e24399f1649f3b0808ab640fc1
Lucas Niño Ruiz <lnino@uji.es>
2024-11-29 13:12:16 +0100

    feat(simulation): added load previous exposures developer option

--- src/app/views/main/modals/simulation/simulation-modal.component.ts
+++ src/app/views/main/modals/simulation/simulation-modal.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { NavigationService } from "../../../navigation.service";
 import { getLogger, Logger } from "~/app/core/utils/logger";
 import { awarns } from "@awarns/core";
+import { ServerApiService } from "~/app/core/server";
+import { AccountService } from "~/app/core/account";
 
 @Component({
     selector: "SymSimulationModal",
@@ -13,7 +15,11 @@ export class SimulationModalComponent {
 
     private logger: Logger;
 
-    constructor(private navigationService: NavigationService) {
+    constructor(
+        private navigationService: NavigationService,
+        private serverClientService: ServerApiService,
+        private accountService: AccountService
+    ) {
         this.logger = getLogger("SimulationModalComponent");
     }
 
@@ -23,5 +29,12 @@ export class SimulationModalComponent {
 
     emitAwarnsFrameworkEvent(eventName: string, data?: any) {
         awarns.emitEvent(eventName, data);
+    }
+
+    async loadPreviousExposures() {
+        await this.serverClientService.queries.getAllRequest(
+            this.accountService.deviceProfile.patientId,
+            this.accountService.deviceProfile.studyId
+        );
     }
 }
