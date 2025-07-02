8aadd9d04166468843b181f4bde3b33135c844d6
Lucas Niño Ruiz <lnino@uji.es>
2025-07-02 15:27:31 +0200

    fix(custom-messages): custom notification messages now working

--- src/app/tasks/feedback/custom-notifications.ts
+++ src/app/tasks/feedback/custom-notifications.ts
@@ -1,6 +1,11 @@
 import { ApplicationSettings } from "@nativescript/core";
 
-import { DispatchableEvent, Task, TaskOutcome, TaskParams } from "@awarns/core/tasks";
+import {
+    DispatchableEvent,
+    Task,
+    TaskOutcome,
+    TaskParams,
+} from "@awarns/core/tasks";
 import { sendNotificationTask } from "@awarns/notifications";
 
 import { getLogger } from "~/app/core/utils/logger";
@@ -14,7 +19,7 @@ type Message = {
     body: string;
 };
 
-// This task is an extension of the sendNotification task. 
+// This task is an extension of the sendNotification task.
 
 // It should be used exactly as default sendNotification task as it will
 // send a custom notification if it is available or possible.
@@ -29,24 +34,32 @@ export class CustomNotification extends Task {
     ): Promise<void | TaskOutcome> {
         try {
             // Get custom message if possible
-            const unparsedMessages = ApplicationSettings.getString(PATIENT_MESSAGES_KEY);
+            const unparsedMessages =
+                ApplicationSettings.getString(PATIENT_MESSAGES_KEY);
             const messages: Message[] = JSON.parse(unparsedMessages);
-            const customMessage: Message = messages.find(m => m.event === invocationEvent.name);
-            
+            const customMessage: Message = messages.find(
+                (m) => m.event === invocationEvent.name
+            );
+
             // Send default message if no custom message was found
             if (customMessage === undefined) {
                 return sendNotificationTask().run(taskParams, invocationEvent);
             }
 
             // Send custom message saved in device's storage (if present)
-            return sendNotificationTask().run({
-                title: customMessage.title,
-                body: customMessage.body,
-                tapAction: taskParams["tapAction"]
-            }, invocationEvent);
+            return sendNotificationTask().run(
+                {
+                    title: customMessage.title,
+                    body: customMessage.body,
+                    tapAction: taskParams["tapAction"],
+                },
+                invocationEvent
+            );
         } catch (e) {
-            getLogger("sendCustomNotificationTask").warn(`Was not possible to send a custom notification. Reason ${e}`)
-            
+            getLogger("sendCustomNotificationTask").warn(
+                `Was not possible to send a custom notification. Reason ${e}`
+            );
+
             // Default message is sent if there is any error
             return sendNotificationTask().run(taskParams, invocationEvent);
         }
